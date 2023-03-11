ca38c5609bec2ea8e74100f00ed1deb8a7ad9686
Daniel Öster <dalathegreat@gmail.com>
2023-03-11 20:37:27 +0200

    Fix 1F2 and 1D4 writing
    Bugfix for SOC% not updating after battery has been turned on

--- Software/BYD-MODBUS-2-LEAF-CAN.ino
+++ Software/BYD-MODBUS-2-LEAF-CAN.ino
@@ -11,7 +11,7 @@
 #define BATTERY_WH_MAX 30000 //Battery size in Wh (Maximum value Fronius accepts is 60000 [60kWh])
 #define MAXPERCENTAGE 800 //80.0% , Max percentage the battery will charge to (App will show 100% once this value is reached)
 #define MINPERCENTAGE 200 //20.0% , Min percentage the battery will discharge to (App will show 0% once this value is reached)
-byte printValues = 0; //Should modbus values be printed to serial output? 
+byte printValues = 1; //Should modbus values be printed to serial output? 
 
 /* Do not change code below unless you are sure what you are doing */
 //CAN parameters
@@ -21,12 +21,14 @@ unsigned long previousMillis100 = 0; // will store last time a 100ms CAN Message
 const int interval10 = 10; // interval (ms) at which send CAN Messages
 const int interval100 = 100; // interval (ms) at which send CAN Messages
 const int rx_queue_size = 10; // Receive Queue size
+uint8_t mprun10r = 0; //counter 0-20 for 0x1F2 message
 byte mprun10 = 0; //counter 0-3
 byte mprun100 = 0; //counter 0-3
 
-CAN_frame_t LEAF_1F2 = {.MsgID = 0x1F2, LEAF_1F2.FIR.B.DLC = 8, LEAF_1F2.FIR.B.FF = CAN_frame_std, LEAF_1F2.data.u8[0] = 0x64, LEAF_1F2.data.u8[1] = 0x64,LEAF_1F2.data.u8[2] = 0x32, LEAF_1F2.data.u8[3] = 0xA0,LEAF_1F2.data.u8[4] = 0x00,LEAF_1F2.data.u8[5] = 0x0A};
+CAN_frame_t LEAF_1F2 = {.MsgID = 0x1F2, LEAF_1F2.FIR.B.DLC = 8, LEAF_1F2.FIR.B.FF = CAN_frame_std, LEAF_1F2.data.u8[0] = 0x10, LEAF_1F2.data.u8[1] = 0x64,LEAF_1F2.data.u8[2] = 0x00, LEAF_1F2.data.u8[3] = 0xB0,LEAF_1F2.data.u8[4] = 0x00,LEAF_1F2.data.u8[5] = 0x1E};
 CAN_frame_t LEAF_50B = {.MsgID = 0x50B, LEAF_50B.FIR.B.DLC = 7, LEAF_50B.FIR.B.FF = CAN_frame_std, LEAF_50B.data.u8[0] = 0x00, LEAF_50B.data.u8[1] = 0x00,LEAF_50B.data.u8[2] = 0x06, LEAF_50B.data.u8[3] = 0xC0,LEAF_50B.data.u8[4] = 0x00,LEAF_50B.data.u8[5] = 0x00};
 CAN_frame_t LEAF_50C = {.MsgID = 0x50C, LEAF_50C.FIR.B.DLC = 8, LEAF_50C.FIR.B.FF = CAN_frame_std, LEAF_50C.data.u8[0] = 0x00, LEAF_50C.data.u8[1] = 0x00,LEAF_50C.data.u8[2] = 0x00, LEAF_50C.data.u8[3] = 0x00,LEAF_50C.data.u8[4] = 0x00,LEAF_50C.data.u8[5] = 0x00};
+CAN_frame_t LEAF_1D4 = {.MsgID = 0x1D4, LEAF_1D4.FIR.B.DLC = 8, LEAF_1D4.FIR.B.FF = CAN_frame_std, LEAF_1D4.data.u8[0] = 0x6E, LEAF_1D4.data.u8[1] = 0x6E,LEAF_1D4.data.u8[2] = 0x00, LEAF_1D4.data.u8[3] = 0x00,LEAF_1D4.data.u8[4] = 0x00,LEAF_1D4.data.u8[5] = 0x00};
 
 //Nissan LEAF battery parameters from CAN
 #define WH_PER_GID 77   //One GID is this amount of Watt hours
@@ -188,21 +190,28 @@ void update_values_leaf_battery()
   }
 	
   /* Define power able to be put into the battery */
-  if(LB_MAX_POWER_FOR_CHARGER > 30) //if >30kW can be put into the battery
+  if(LB_Charge_Power_Limit > 30) //if >30kW can be put into the battery
   {
     max_target_charge_power = 30000; //cap value so we don't go over the Fronius limits
   }
-  if(LB_MAX_POWER_FOR_CHARGER < 0) //LB_MAX_POWER_FOR_CHARGER can actually go to -10kW
+  if(LB_Charge_Power_Limit < 0) //LB_MAX_POWER_FOR_CHARGER can actually go to -10kW
   {
     max_target_charge_power = 0; //cap calue so we dont do under the Fronius limits
   }
   else
   {
-    max_target_charge_power = (LB_MAX_POWER_FOR_CHARGER * 1000); //kW to W
+    max_target_charge_power = (LB_Charge_Power_Limit * 1000); //kW to W
   }
   if(SOC == 10000) //Scaled SOC% value is 100.00%
   {
     max_target_charge_power = 0; //No need to charge further, set max power to 0
+  }
+
+  /*Extra safeguards*/
+  if(LB_GIDS < 10) //1kWh left in battery
+  { //Battery is running abnormally low, some other safety might have failed. Zero it all out.
+    SOC = 0;
+    max_target_discharge_power = 0;
   }
 	
   LB_Power = LB_Total_Voltage * LB_Current;//P = U * I
@@ -219,16 +228,19 @@ void update_values_leaf_battery()
   {  //values heading towards the modbus registers
     Serial.println("SOH%: ");
     Serial.println(SOH);
-    Serial.println("SOC%: ");
+    Serial.println("SOC% to Fronius: ");
     Serial.println(SOC);
     Serial.println("Max discharge power: ");
     Serial.println(max_target_discharge_power);
-    Serial.println("Max discharge power: ");
+    Serial.println("Max charge power: ");
     Serial.println(max_target_charge_power);
     Serial.println("Temperature Min: ");
     Serial.println(temperature_min);
     Serial.println("Temperature Max: ");
     Serial.println(temperature_max);
+    Serial.println("GIDS: ");
+    Serial.println(LB_GIDS);
+    
   }
 }
 
@@ -336,8 +348,8 @@ void handle_can_leaf_battery()
           // negative so extend the sign bit
           LB_Current |= 0xf800;
         }
-        Serial.println("LB_Current: ");
-        Serial.println(LB_Current);
+        //Serial.println("LB_Current: ");
+        //Serial.println(LB_Current);
 
 				LB_Total_Voltage = ((rx_frame.data.u8[2] << 2) | (rx_frame.data.u8[3] & 0xc0) >> 6) / 2;
         
@@ -439,31 +451,153 @@ void handle_can_leaf_battery()
 
     if(mprun10 == 0)
     {
-      LEAF_1F2.data.u8[6] = 0x00;
-      LEAF_1F2.data.u8[7] = 0x8F;
+      LEAF_1D4.data.u8[4] = 0x07;
+      LEAF_1D4.data.u8[5] = 0x46;
+      LEAF_1D4.data.u8[6] = 0x01;
+      LEAF_1D4.data.u8[7] = 0x44;
     }
     else if(mprun10 == 1)
     {
-      LEAF_1F2.data.u8[6] = 0x01;
-      LEAF_1F2.data.u8[7] = 0x80;
+      LEAF_1D4.data.u8[4] = 0x47;
+      LEAF_1D4.data.u8[5] = 0x46;
+      LEAF_1D4.data.u8[6] = 0x01;
+      LEAF_1D4.data.u8[7] = 0x44;
     }
     else if(mprun10 == 2)
     {
-      LEAF_1F2.data.u8[6] = 0x02;
-      LEAF_1F2.data.u8[7] = 0x81;
+      LEAF_1D4.data.u8[4] = 0x87;
+      LEAF_1D4.data.u8[5] = 0x46;
+      LEAF_1D4.data.u8[6] = 0x01;
+      LEAF_1D4.data.u8[7] = 0x88;
     }
     else if(mprun10 == 3)
     {
-      LEAF_1F2.data.u8[6] = 0x03;
-      LEAF_1F2.data.u8[7] = 0x82;
+      LEAF_1D4.data.u8[4] = 0xC7;
+      LEAF_1D4.data.u8[5] = 0x46;
+      LEAF_1D4.data.u8[6] = 0x01;
+      LEAF_1D4.data.u8[7] = 0x4F;
     }
-    ESP32Can.CANWriteFrame(&LEAF_1F2); //Contains (CHG_STA_RQ == 1 == Normal Charge)
+    ESP32Can.CANWriteFrame(&LEAF_1D4); 
 
 		mprun10++;
 		if (mprun10 > 3)
 		{
 			mprun10 = 0;
 		}
+
+    switch(mprun10r)
+    {
+		case(0):	
+			LEAF_1F2.data.u8[3] = 0xB0;
+			LEAF_1F2.data.u8[6] = 0x00;
+			LEAF_1F2.data.u8[7] = 0x8F;
+			break;
+		case(1):
+			LEAF_1F2.data.u8[3] = 0xB0;
+			LEAF_1F2.data.u8[6] = 0x01;
+			LEAF_1F2.data.u8[7] = 0x80;
+			break;
+		case(2):
+			LEAF_1F2.data.u8[3] = 0xB0;
+			LEAF_1F2.data.u8[6] = 0x02;
+			LEAF_1F2.data.u8[7] = 0x81;
+			break;
+		case(3):
+			LEAF_1F2.data.u8[3] = 0xB0;
+			LEAF_1F2.data.u8[6] = 0x03;
+			LEAF_1F2.data.u8[7] = 0x82;
+			break;
+		case(4):
+			LEAF_1F2.data.u8[3] = 0xB0;
+			LEAF_1F2.data.u8[6] = 0x00;
+			LEAF_1F2.data.u8[7] = 0x8F;
+			break;
+		case(5):	// Set 2
+			LEAF_1F2.data.u8[3] = 0xB4;
+			LEAF_1F2.data.u8[6] = 0x01;
+			LEAF_1F2.data.u8[7] = 0x84;
+			break;
+		case(6):
+			LEAF_1F2.data.u8[3] = 0xB4;
+			LEAF_1F2.data.u8[6] = 0x02;
+			LEAF_1F2.data.u8[7] = 0x85;
+			break;
+		case(7):
+			LEAF_1F2.data.u8[3] = 0xB4;
+			LEAF_1F2.data.u8[6] = 0x03;
+			LEAF_1F2.data.u8[7] = 0x86;
+			break;
+		case(8):
+			LEAF_1F2.data.u8[3] = 0xB4;
+			LEAF_1F2.data.u8[6] = 0x00;
+			LEAF_1F2.data.u8[7] = 0x83;
+			break;
+		case(9):
+			LEAF_1F2.data.u8[3] = 0xB4;
+			LEAF_1F2.data.u8[6] = 0x01;
+			LEAF_1F2.data.u8[7] = 0x84;
+			break;
+		case(10):	// Set 3
+			LEAF_1F2.data.u8[3] = 0xB0;
+			LEAF_1F2.data.u8[6] = 0x02;
+			LEAF_1F2.data.u8[7] = 0x81;
+			break;
+		case(11):
+			LEAF_1F2.data.u8[3] = 0xB0;
+			LEAF_1F2.data.u8[6] = 0x03;
+			LEAF_1F2.data.u8[7] = 0x82;
+			break;
+		case(12):
+			LEAF_1F2.data.u8[3] = 0xB0;
+			LEAF_1F2.data.u8[6] = 0x00;
+			LEAF_1F2.data.u8[7] = 0x8F;
+			break;
+		case(13):
+			LEAF_1F2.data.u8[3] = 0xB0;
+			LEAF_1F2.data.u8[6] = 0x01;
+			LEAF_1F2.data.u8[7] = 0x80;
+			break;
+		case(14):
+			LEAF_1F2.data.u8[3] = 0xB0;
+			LEAF_1F2.data.u8[6] = 0x02;
+			LEAF_1F2.data.u8[7] = 0x81;
+			break;
+		case(15):	// Set 4
+			LEAF_1F2.data.u8[3] = 0xB4;
+			LEAF_1F2.data.u8[6] = 0x03;
+			LEAF_1F2.data.u8[7] = 0x86;
+			break;
+		case(16):
+			LEAF_1F2.data.u8[3] = 0xB4;
+			LEAF_1F2.data.u8[6] = 0x00;
+			LEAF_1F2.data.u8[7] = 0x83;
+			break;
+		case(17):
+			LEAF_1F2.data.u8[3] = 0xB4;
+			LEAF_1F2.data.u8[6] = 0x01;
+			LEAF_1F2.data.u8[7] = 0x84;
+			break;
+		case(18):
+			LEAF_1F2.data.u8[3] = 0xB4;
+			LEAF_1F2.data.u8[6] = 0x02;
+			LEAF_1F2.data.u8[7] = 0x85;
+			break;
+		case(19):
+			LEAF_1F2.data.u8[3] = 0xB4;
+			LEAF_1F2.data.u8[6] = 0x03;
+			LEAF_1F2.data.u8[7] = 0x86;
+			break;
+		default:
+			break;
+	  }
+
+    ESP32Can.CANWriteFrame(&LEAF_1F2); //Contains (CHG_STA_RQ == 1 == Normal Charge)
+
+    mprun10r++;
+    if(mprun10r > 19)	// 0x1F2 patter repeats after 20 messages,
+    {
+    mprun10r = 0;
+    }
 		//Serial.println("CAN 10ms done");
 	}
 }
