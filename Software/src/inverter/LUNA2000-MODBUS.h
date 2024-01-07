c177587f1347b5052c71ce4a3f3a34d1d643143d
Daniel <dalathegreat@gmail.com>
2024-01-08 00:00:02 +0200

    Fix compilation error for Luna2000

--- Software/src/inverter/LUNA2000-MODBUS.h
+++ Software/src/inverter/LUNA2000-MODBUS.h
@@ -19,7 +19,6 @@ extern uint8_t bms_char_dis_status;
 extern uint16_t stat_batt_power;
 extern uint16_t temperature_min;
 extern uint16_t temperature_max;
-extern uint16_t capacity_Wh_startup;
 extern uint16_t max_power;
 extern uint16_t max_voltage;
 extern uint16_t min_voltage;
