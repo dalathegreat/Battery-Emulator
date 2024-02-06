6bc253ff6e9c2ca7c3b900411c8e057b64fbfad8
Cabooman <81711263+Cabooman@users.noreply.github.com>
2024-02-06 08:32:53 +0100

    Comments

--- test/utils/timer_test.cpp
+++ test/utils/timer_test.cpp
@@ -11,25 +11,32 @@ TEST(timer_test) {
   unsigned long test_interval = 10;
   bool result;
 
+  // Create a timer, assert that it hasn't elapsed immediately
   testlib_millis = 30;
   MyTimer timer(test_interval);
   result = timer.elapsed();
   ASSERT_EQ(result, false);
 
+  // Test interval - 1, shouldn't have elapsed
   testlib_millis += test_interval - 1;
   result = timer.elapsed();
   ASSERT_EQ(result, false);
 
+  // Add 1, so now it should have elapsed
   testlib_millis += 1;
   result = timer.elapsed();
   ASSERT_EQ(result, true);
+
+  // The timer should have reset when it elapsed
   result = timer.elapsed();
   ASSERT_EQ(result, false);
 
+  // Test close to the next interval
   testlib_millis += test_interval - 1;
   result = timer.elapsed();
   ASSERT_EQ(result, false);
 
+  // Add 1, ensure that the timer elapses but only once
   testlib_millis += 1;
   result = timer.elapsed();
   ASSERT_EQ(result, true);
