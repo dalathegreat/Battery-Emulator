77f74ee4ccf2f2bf21b6d15c263e55869a496aae
Ernest Cunningham <ernest@ging.nz>
2024-03-11 22:24:42 +1300

    Add mDNS for local device discovery.

--- Software/Software.ino
+++ Software/Software.ino
@@ -20,6 +20,7 @@
 
 #ifdef WEBSERVER
 #include "src/devboard/webserver/webserver.h"
+#include <ESPmDNS.h>
 #endif
 
 Preferences settings;                   // Store user settings
@@ -147,6 +148,8 @@ void setup() {
   inform_user_on_inverter();
 
   init_battery();
+  
+  init_mDNS();
 
   // BOOT button at runtime is used as an input for various things
   pinMode(0, INPUT_PULLUP);
@@ -204,6 +207,23 @@ void loop() {
     test_all_colors = false;
   } else {
     test_all_colors = true;
+  }
+}
+
+// Initialise mDNS
+void init_mDNS() {
+  
+  // Calulate the host name using the last two chars from the MAC address so each one is likely unique on a network.
+  // e.g batteryemulator8C.local where the mac address is 08:F9:E0:D1:06:8C
+  String mac = WiFi.macAddress();
+  String mdnsHost = "batteryemulator"+ mac.substring( mac.length() - 2 );
+
+  // Initialize mDNS .local resolution
+  if (!MDNS.begin(mdnsHost)) {
+    Serial.println("Error setting up MDNS responder!");
+  } else {
+    // Advertise via bonjour the service so we can auto discover these battery emulators on the local network.
+    MDNS.addService("battery_emulator", "tcp", 80);
   }
 }
 
