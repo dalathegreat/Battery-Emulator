0c984d906e97afef144d8ed14bbe7e583da3273e
Daniel <dalathegreat@gmail.com>
2024-03-27 21:18:12 +0200

    Wrap usb events in ifdef to reduce cpu usage

--- Software/src/devboard/webserver/events_html.cpp
+++ Software/src/devboard/webserver/events_html.cpp
@@ -22,10 +22,12 @@ String events_processor(const String& var) {
     for (int i = 0; i < EVENT_NOF_EVENTS; i++) {
       event_pointer = get_event_pointer((EVENTS_ENUM_TYPE)i);
       EVENTS_ENUM_TYPE event_handle = static_cast<EVENTS_ENUM_TYPE>(i);
+#ifdef DEBUG_VIA_USB
       Serial.println("Event: " + String(get_event_enum_string(event_handle)) +
                      " count: " + String(event_pointer->occurences) + " seconds: " + String(event_pointer->timestamp) +
                      " data: " + String(event_pointer->data) +
                      " level: " + String(get_event_level_string(event_handle)));
+#endif
       if (event_pointer->occurences > 0) {
         content.concat("<div class='event'>");
         content.concat("<div>" + String(get_event_enum_string(event_handle)) + "</div>");
