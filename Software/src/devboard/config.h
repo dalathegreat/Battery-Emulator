a70a6970aa8df8ec41747a6f8c28f9fbbfb1bfde
Daniel <dalathegreat@gmail.com>
2024-04-03 23:43:22 +0300

    Pre-commit fix

--- Software/src/devboard/config.h
+++ Software/src/devboard/config.h
@@ -21,11 +21,11 @@
 #endif
 
 #ifdef CAN_FD
-#define MCP2517_SCK 12 // SCK input of MCP2517
-#define MCP2517_SDI 5 // SDI input of MCP2517
-#define MCP2517_SDO 34 // SDO output of MCP2517
-#define MCP2517_CS 18 // CS input of MCP2517
-#define MCP2517_INT 35 // INT output of MCP2517
+#define MCP2517_SCK 12  // SCK input of MCP2517
+#define MCP2517_SDI 5   // SDI input of MCP2517
+#define MCP2517_SDO 34  // SDO output of MCP2517
+#define MCP2517_CS 18   // CS input of MCP2517
+#define MCP2517_INT 35  // INT output of MCP2517
 #endif
 
 #ifdef CONTACTOR_CONTROL
