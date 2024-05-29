12f440126bd3c030c9db031f75167f1308d4c3a0
Daniel <dalathegreat@gmail.com>
2024-05-29 13:29:53 +0300

    Fix compilation issue

--- Software/src/lib/miwagner-ESP32-Arduino-CAN/CAN.c
+++ Software/src/lib/miwagner-ESP32-Arduino-CAN/CAN.c
@@ -34,7 +34,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 
-#include "esp_intr.h"
+#include "esp_intr_alloc.h" // Renamed when migrating ESP32 2.x -> 3.x
 #include "soc/dport_reg.h"
 #include <math.h>
 
@@ -42,6 +42,10 @@
 
 #include "can_regdef.h"
 #include "CAN_config.h"
+
+#define TWAI_TX_IDX 123 // TODO: Are these OK? 
+// not sure what file is needed now, maybe "soc/gpio_sig_map.h" but using hard coded values for now
+#define TWAI_RX_IDX 94  // TODO: Are these OK? 
 
 // CAN Filter - no acceptance filter
 static CAN_filter_t __filter = { Dual_Mode, 0, 0, 0, 0, 0Xff, 0Xff, 0Xff, 0Xff };
@@ -177,12 +181,12 @@ int CAN_init() {
 	// configure TX pin
 	gpio_set_level(CAN_cfg.tx_pin_id, 1);
 	gpio_set_direction(CAN_cfg.tx_pin_id, GPIO_MODE_OUTPUT);
-	gpio_matrix_out(CAN_cfg.tx_pin_id, CAN_TX_IDX, 0, 0);
+	gpio_matrix_out(CAN_cfg.tx_pin_id, TWAI_TX_IDX, 0, 0);
 	gpio_pad_select_gpio(CAN_cfg.tx_pin_id);
 
 	// configure RX pin
 	gpio_set_direction(CAN_cfg.rx_pin_id, GPIO_MODE_INPUT);
-	gpio_matrix_in(CAN_cfg.rx_pin_id, CAN_RX_IDX, 0);
+	gpio_matrix_in(CAN_cfg.rx_pin_id, TWAI_RX_IDX, 0);
 	gpio_pad_select_gpio(CAN_cfg.rx_pin_id);
 
 	// set to PELICAN mode
