1600cecb76990143cc0f8f54596395ad4c9081bb
Daniel Öster <dalathegreat@gmail.com>
2024-06-02 21:42:39 +0300

    Update version number for release

--- Software/Software.ino
+++ Software/Software.ino
@@ -33,7 +33,7 @@
 
 Preferences settings;  // Store user settings
 // The current software version, shown on webserver
-const char* version_number = "6.0.RC";
+const char* version_number = "6.0.0";
 
 // Interval settings
 uint16_t intervalUpdateValues = INTERVAL_5_S;  // Interval at which to update inverter values / Modbus registers
