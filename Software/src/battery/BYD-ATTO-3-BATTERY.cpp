1b0b12f1c01b6c428f1f5eff24b718d01e902d1b
Daniel <dalathegreat@gmail.com>
2024-06-05 23:07:49 +0300

    Pre-commit fix

--- Software/src/battery/BYD-ATTO-3-BATTERY.cpp
+++ Software/src/battery/BYD-ATTO-3-BATTERY.cpp
@@ -67,65 +67,65 @@ void update_values_battery() {  //This function maps all the values fetched via 
 
 void receive_can_battery(CAN_frame_t rx_frame) {
   datalayer.battery.status.CAN_battery_still_alive = CAN_STILL_ALIVE;
-  switch (rx_frame.MsgID) { //Log values taken with 422V from battery
-    case 0x244: //00,00,00,04,41,0F,20,8B - 0x441 = 1089 , 0xF20 = 3872 , 0x410F / 4 = 4163
+  switch (rx_frame.MsgID) {  //Log values taken with 422V from battery
+    case 0x244:              //00,00,00,04,41,0F,20,8B - 0x441 = 1089 , 0xF20 = 3872 , 0x410F / 4 = 4163
       break;
-    case 0x245: //01,00,02,19,3A,25,90,F4
-                //02,00,90,01,79,79,90,EA
-                //03,C6,88,12,FD,48,90,5C
-                //04,00,FF,FF,00,00,90,6D
+    case 0x245:  //01,00,02,19,3A,25,90,F4
+                 //02,00,90,01,79,79,90,EA
+                 //03,C6,88,12,FD,48,90,5C
+                 //04,00,FF,FF,00,00,90,6D
       break;
-    case 0x286: //01,FF,FF,FF,FF,FF,FF,04
+    case 0x286:  //01,FF,FF,FF,FF,FF,FF,04
       break;
-    case 0x334: //FF,FF,FF,FC,3F,00,F0,D7
+    case 0x334:  //FF,FF,FF,FC,3F,00,F0,D7
       break;
-    case 0x338: //01,52,02,00,88,13,00,0F
+    case 0x338:  //01,52,02,00,88,13,00,0F
       break;
-    case 0x344: //00,52,02,CC,1F,FF,04,BD
+    case 0x344:  //00,52,02,CC,1F,FF,04,BD
       break;
-    case 0x345: //27,0B,00,00,00,E0,01,EC
+    case 0x345:  //27,0B,00,00,00,E0,01,EC
       break;
-    case 0x347: //FF,00,00,F9,FF,FF,FF,0A
+    case 0x347:  //FF,00,00,F9,FF,FF,FF,0A
       break;
-    case 0x34A: //00,52,02,CC,1F,FF,04,BD
+    case 0x34A:  //00,52,02,CC,1F,FF,04,BD
       break;
-    case 0x35E: //01,00,C8,32,00,63,00,A1 - Flickering between A0 and A1, Could be temperature?
+    case 0x35E:  //01,00,C8,32,00,63,00,A1 - Flickering between A0 and A1, Could be temperature?
       break;
-    case 0x360: //30,19,DE,D1,0B,C3,4B,EE - Last and first byte has F-0 counters - 0x10BC (4284) 
+    case 0x360:  //30,19,DE,D1,0B,C3,4B,EE - Last and first byte has F-0 counters - 0x10BC (4284)
       break;
-    case 0x36C: //01,57,13,DC,08,70,17,29
-                //02,03,DC,05,C0,0F,0F,3B
-                //03,86,01,40,06,5C,02,D1
-                //04,57,13,73,04,01,FF,1A
-                //05,FF,FF,FF,FF,FF,FF,00
+    case 0x36C:  //01,57,13,DC,08,70,17,29
+                 //02,03,DC,05,C0,0F,0F,3B
+                 //03,86,01,40,06,5C,02,D1
+                 //04,57,13,73,04,01,FF,1A
+                 //05,FF,FF,FF,FF,FF,FF,00
       break;
-    case 0x438: //55,55,01,F6,47,2E,10,D9 - 0x10D9 = 4313
+    case 0x438:  //55,55,01,F6,47,2E,10,D9 - 0x10D9 = 4313
       break;
-    case 0x43A: //7E,0A,B0,1C,63,E1,03,64
+    case 0x43A:  //7E,0A,B0,1C,63,E1,03,64
       break;
-    case 0x43B: //01,3B,06,39,FF,64,64,BD
+    case 0x43B:  //01,3B,06,39,FF,64,64,BD
       break;
-    case 0x43C: //00,3B,3A,3A,3A,3A,39,A3
-                //01,3A,3A,39,3A,FF,FF,19
+    case 0x43C:  //00,3B,3A,3A,3A,3A,39,A3
+                 //01,3A,3A,39,3A,FF,FF,19
       break;
-    case 0x43D: //Varies a lot
+    case 0x43D:  //Varies a lot
       break;
-    case 0x444: //9E,01,88,13,64,64,98,65
+    case 0x444:  //9E,01,88,13,64,64,98,65
       break;
-    case 0x445: //00,98,FF,FF,63,20,4E,98
+    case 0x445:  //00,98,FF,FF,63,20,4E,98
       break;
-    case 0x446: //2C,D4,0C,4D,21,DC,0C,9D - 0,1,7th frame varies a lot
+    case 0x446:  //2C,D4,0C,4D,21,DC,0C,9D - 0,1,7th frame varies a lot
       break;
-    case 0x447: //06,39,01,3B,E0,03,3A,67
+    case 0x447:  //06,39,01,3B,E0,03,3A,67
       break;
-    case 0x47B: //01,FF,FF,FF,FF,FF,FF,FF
+    case 0x47B:  //01,FF,FF,FF,FF,FF,FF,FF
       break;
-    case 0x524: //24,40,00,00,00,00,00,9B
+    case 0x524:                                                            //24,40,00,00,00,00,00,9B
       datalayer.battery.status.CAN_battery_still_alive = CAN_STILL_ALIVE;  // Let system know battery is sending CAN
       break;
     default:
       break;
-}
+  }
 }
 void send_can_battery() {
   unsigned long currentMillis = millis();
