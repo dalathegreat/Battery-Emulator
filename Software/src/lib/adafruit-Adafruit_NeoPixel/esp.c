47aabca8b3457e1d7e8e3fd15fb170bf177aeefb
Daniel Öster <dalathegreat@gmail.com>
2024-06-30 21:57:53 +0300

    Remove commented out codeblocks

--- Software/src/lib/adafruit-Adafruit_NeoPixel/esp.c
+++ Software/src/lib/adafruit-Adafruit_NeoPixel/esp.c
@@ -158,13 +158,6 @@ void espShow(uint8_t pin, uint8_t* pixels, uint32_t numBytes, boolean is800KHz) 
 
   // Write and wait to finish
   rmt_write_sample(0, pixels, (size_t)numBytes, false);
-  // rmt_wait_tx_done(config.channel, pdMS_TO_TICKS(100));
-
-  // Free channel again
-  // rmt_driver_uninstall(config.channel);
-  // rmt_reserved_channels[channel] = false;
-
-  // gpio_set_direction(pin, GPIO_MODE_OUTPUT);
 }
 
 #endif
