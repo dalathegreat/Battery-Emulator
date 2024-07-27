866bc8e1e102235775ce8bfbfaf9f22b20481fd8
LevinSwe <levin_m@hotmail.com>
2024-07-27 14:11:32 +0200

    Update PYLON-CAN.cpp
    - Add Sleep status to open contactors in inverter at Fault
    - Change status-value from 0x 11, 12, 13 to 01, 02, 03 to not call for extra chargning.

--- Software/src/inverter/PYLON-CAN.cpp
+++ Software/src/inverter/PYLON-CAN.cpp
@@ -240,12 +240,14 @@ void update_values_can_inverter() {  //This function maps all the values fetched
   PYLON_4211.data.u8[7] = (datalayer.battery.status.soh_pptt / 100);
 
   // Status=Bit 0,1,2= 0:Sleep, 1:Charge, 2:Discharge 3:Idle. Bit3 ForceChargeReq. Bit4 Balance charge Request
-  if (datalayer.battery.status.current_dA < 0) {
-    PYLON_4251.data.u8[0] = (0x11);  // Charge
+  if (datalayer.battery.status.bms_status == FAULT) {
+    PYLON_4251.data.u8[0] = (0x00);  // Sleep
+  } else if (datalayer.battery.status.current_dA < 0) {
+    PYLON_4251.data.u8[0] = (0x01);  // Charge
   } else if (datalayer.battery.status.current_dA > 0) {
-    PYLON_4251.data.u8[0] = (0x12);  // Discharge
+    PYLON_4251.data.u8[0] = (0x02);  // Discharge
   } else if (datalayer.battery.status.current_dA == 0) {
-    PYLON_4251.data.u8[0] = (0x13);  // Idle
+    PYLON_4251.data.u8[0] = (0x03);  // Idle
   }
 
 #ifdef INVERT_LOW_HIGH_BYTES  //Useful for Sofar inverters
