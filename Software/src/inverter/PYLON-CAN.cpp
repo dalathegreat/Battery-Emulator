3061bea4369e2510f5b76301138423343f8f7d99
Daniel Öster <dalathegreat@gmail.com>
2024-08-11 21:03:16 +0300

    Change defaults to Force H2, code review

--- Software/src/inverter/PYLON-CAN.cpp
+++ Software/src/inverter/PYLON-CAN.cpp
@@ -11,11 +11,11 @@
 
 /* Some inverters need to see a specific amount of cells/modules to emulate a specific Pylon battery.
 Change the following only if your inverter is generating fault codes about voltage range */
-#define BATTERY_MODULE_AMOUNT 75
-#define MODULES_IN_SERIES 5
+#define TOTAL_CELL_AMOUNT 120
+#define MODULES_IN_SERIES 4
 #define CELLS_PER_MODULE 30
-#define VOLTAGE_LEVEL 192
-#define AH_CAPACITY 86
+#define VOLTAGE_LEVEL 384
+#define AH_CAPACITY 37
 
 /* Do not change code below unless you are sure what you are doing */
 //Actual content messages
@@ -24,12 +24,25 @@ CAN_frame PYLON_7310 = {.FD = false,
                         .DLC = 8,
                         .ID = 0x7310,
                         .data = {0x01, 0x00, 0x02, 0x01, 0x01, 0x02, 0x00, 0x00}};
-CAN_frame PYLON_7320 = {.FD = false,
+CAN_frame PYLON_7311 = {.FD = false,
                         .ext_ID = true,
                         .DLC = 8,
-                        .ID = 0x7320,
-                        .data = {BATTERY_MODULE_AMOUNT, (uint8_t)(BATTERY_MODULE_AMOUNT >> 8), MODULES_IN_SERIES, CELLS_PER_MODULE,
-             VOLTAGE_LEVEL, (uint8_t)(VOLTAGE_LEVEL >> 8), AH_CAPACITY, (uint8_t)(AH_CAPACITY >> 8)}};
+                        .ID = 0x7311,
+                        .data = {0x01, 0x00, 0x02, 0x01, 0x01, 0x02, 0x00, 0x00}};
+CAN_frame PYLON_7320 = {
+    .FD = false,
+    .ext_ID = true,
+    .DLC = 8,
+    .ID = 0x7320,
+    .data = {TOTAL_CELL_AMOUNT, (uint8_t)(TOTAL_CELL_AMOUNT >> 8), MODULES_IN_SERIES, CELLS_PER_MODULE, VOLTAGE_LEVEL,
+             (uint8_t)(VOLTAGE_LEVEL >> 8), AH_CAPACITY, (uint8_t)(AH_CAPACITY >> 8)}};
+CAN_frame PYLON_7321 = {
+    .FD = false,
+    .ext_ID = true,
+    .DLC = 8,
+    .ID = 0x7321,
+    .data = {TOTAL_CELL_AMOUNT, (uint8_t)(TOTAL_CELL_AMOUNT >> 8), MODULES_IN_SERIES, CELLS_PER_MODULE, VOLTAGE_LEVEL,
+             (uint8_t)(VOLTAGE_LEVEL >> 8), AH_CAPACITY, (uint8_t)(AH_CAPACITY >> 8)}};
 CAN_frame PYLON_4210 = {.FD = false,
                         .ext_ID = true,
                         .DLC = 8,
@@ -75,18 +88,6 @@ CAN_frame PYLON_4290 = {.FD = false,
                         .DLC = 8,
                         .ID = 0x4290,
                         .data = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}};
-
-CAN_frame PYLON_7311 = {.FD = false,
-                        .ext_ID = true,
-                        .DLC = 8,
-                        .ID = 0x7311,
-                        .data = {0x01, 0x00, 0x02, 0x01, 0x01, 0x02, 0x00, 0x00}};
-CAN_frame PYLON_7321 = {.FD = false,
-                        .ext_ID = true,
-                        .DLC = 8,
-                        .ID = 0x7321,
-                        .data = {BATTERY_MODULE_AMOUNT, (uint8_t)(BATTERY_MODULE_AMOUNT >> 8), MODULES_IN_SERIES, CELLS_PER_MODULE,
-             VOLTAGE_LEVEL, (uint8_t)(VOLTAGE_LEVEL >> 8), AH_CAPACITY, (uint8_t)(AH_CAPACITY >> 8)}};
 CAN_frame PYLON_4211 = {.FD = false,
                         .ext_ID = true,
                         .DLC = 8,
