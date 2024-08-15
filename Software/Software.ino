c52f0230b5b0903087941c3f974e4515188d0fd5
Daniel Öster <dalathegreat@gmail.com>
2024-08-15 13:23:06 +0300

    Update version number to signal dev work

--- Software/Software.ino
+++ Software/Software.ino
@@ -32,7 +32,7 @@
 
 Preferences settings;  // Store user settings
 // The current software version, shown on webserver
-const char* version_number = "7.0.0";
+const char* version_number = "7.1.dev";
 
 // Interval settings
 uint16_t intervalUpdateValues = INTERVAL_5_S;  // Interval at which to update inverter values / Modbus registers
