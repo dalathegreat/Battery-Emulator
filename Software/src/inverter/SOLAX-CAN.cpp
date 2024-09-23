2cecc3d55a2ccdb335d0f93613689e241eb9ef88
Daniel Öster <dalathegreat@gmail.com>
2024-09-23 23:22:15 +0300

    Add inverter missing from CAN event, Solax

--- Software/src/inverter/SOLAX-CAN.cpp
+++ Software/src/inverter/SOLAX-CAN.cpp
@@ -18,6 +18,7 @@ static unsigned long LastFrameTime = 0;
 static uint8_t number_of_batteries = 1;
 static uint16_t capped_capacity_Wh;
 static uint16_t capped_remaining_capacity_Wh;
+static uint16_t inverter_missing_on_can = 0;
 
 //CAN message translations from this amazing repository: https://github.com/rand12345/solax_can_bus
 
@@ -88,6 +89,12 @@ void update_values_can_inverter() {  //This function maps all the values fetched
   if (millis() - LastFrameTime >= SolaxTimeout) {
     datalayer.system.status.inverter_allows_contactor_closing = false;
     STATE = BATTERY_ANNOUNCE;
+    inverter_missing_on_can++;
+    if (inverter_missing_on_can > CAN_STILL_ALIVE) {
+      set_event(EVENT_CAN_INVERTER_MISSING, 0);
+    } else {
+      clear_event(EVENT_CAN_INVERTER_MISSING);
+    }
   }
   //Calculate the required values
   temperature_average =
