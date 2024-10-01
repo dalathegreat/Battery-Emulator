af9886ad33ca41926bff746acb760c7aecafd4a4
Daniel Öster <dalathegreat@gmail.com>
2024-10-01 10:46:57 +0300

    Update Software.ino for release

--- Software/Software.ino
+++ Software/Software.ino
@@ -50,7 +50,7 @@
 
 Preferences settings;  // Store user settings
 // The current software version, shown on webserver
-const char* version_number = "7.4.dev";
+const char* version_number = "7.4.0";
 
 // Interval settings
 uint16_t intervalUpdateValues = INTERVAL_5_S;  // Interval at which to update inverter values / Modbus registers
