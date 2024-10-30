450d690edf34f84a59f9cf2e30a9d9e55edbdf95
wjcloudy <56305354+wjcloudy@users.noreply.github.com>
2024-10-30 21:41:11 +0000

    0.2alpha version - Update advanced_battery_html.cpp

--- Software/src/devboard/webserver/advanced_battery_html.cpp
+++ Software/src/devboard/webserver/advanced_battery_html.cpp
@@ -18,7 +18,29 @@ String advanced_battery_processor(const String& var) {
 
 
 #ifdef BMW_IX_BATTERY
-    content += "<h4>T30 Terminal Voltage: " + String(datalayer_extended.bmwix.T30_Voltage) + "mV</h4>";
+    content += "<h4>T30 Terminal Voltage: " + String(datalayer_extended.bmwix.T30_Voltage) + " mV</h4>";
+    content += "<h4>Detected Cell Count: " + String(datalayer.battery.info.number_of_cells) + "</h4>";
+     static const char* balanceText[5] = {"0 No balancing mode active",
+                                            "1 Voltage-Controlled Balancing Mode",
+                                            "2 Time-Controlled Balancing Mode with Demand Calculation at End of Charging" ,
+                                            "3 Time-Controlled Balancing Mode with Demand Calculation at Resting Voltage" ,
+                                            "4 No balancing mode active, qualifier invalid"
+                                            };
+    content += "<h4>Balancing Status: " + String((balanceText[datalayer_extended.bmwix.balancing_status])) + "</h4>";
+    static const char* hvilText[2] = {"Error (Loop Open)",
+                                            "OK (Loop Closed)"};
+    content += "<h4>HVIL Status: " + String(hvilText[datalayer_extended.bmwix.hvil_status]) + "</h4>";
+    content += "<h4>BMS Uptime: " + String(datalayer_extended.bmwix.bms_uptime) + " seconds</h4>";
+    content += "<h4>BMS Allowed Charge Amps: " + String(datalayer_extended.bmwix.allowable_charge_amps) + " A</h4>";
+    content += "<h4>BMS Allowed Disharge Amps: " + String(datalayer_extended.bmwix.allowable_discharge_amps) + " A</h4>";
+    content += "<br>";
+    content += "<h3>HV Isolation (2147483647kOhm = maximum/invalid)</h3>";
+    content += "<h4>Isolation Positive: " + String(datalayer_extended.bmwix.iso_safety_positive) + " kOhm</h4>";
+    content += "<h4>Isolation Negative: " + String(datalayer_extended.bmwix.iso_safety_negative) + " kOhm</h4>";
+    content += "<h4>Isolation Parallel: " + String(datalayer_extended.bmwix.iso_safety_parallel) + " kOhm</h4>";
+    content += "<h4>Pyro Status PSS1: " + String(datalayer_extended.bmwix.pyro_status_pss1) + "</h4>";
+    content += "<h4>Pyro Status PSS4: " + String(datalayer_extended.bmwix.pyro_status_pss4) + "</h4>";
+    content += "<h4>Pyro Status PSS6: " + String(datalayer_extended.bmwix.pyro_status_pss6) + "</h4>";
 #endif  //BMW_IX_BATTERY
 
 #ifdef BMW_I3_BATTERY
