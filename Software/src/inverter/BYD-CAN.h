9051293c43887253f7f8fec256e9bc7460c7864a
Daniel Öster <dalathegreat@gmail.com>
2024-11-06 15:57:20 +0200

    Update BYD FW to 3.29

--- Software/src/inverter/BYD-CAN.h
+++ Software/src/inverter/BYD-CAN.h
@@ -3,6 +3,8 @@
 #include "../include.h"
 
 #define CAN_INVERTER_SELECTED
+#define FW_MAJOR_VERSION 0x03
+#define FW_MINOR_VERSION 0x29
 
 void send_intial_data();
 void transmit_can(CAN_frame* tx_frame, int interface);
