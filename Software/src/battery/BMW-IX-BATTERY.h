00f78ce76cf0682704779d57b3743709c8332ce5
wjcloudy <56305354+wjcloudy@users.noreply.github.com>
2024-11-08 12:08:08 +0000

    Update formatting

--- Software/src/battery/BMW-IX-BATTERY.h
+++ Software/src/battery/BMW-IX-BATTERY.h
@@ -12,9 +12,10 @@
 #define MAX_CELL_VOLTAGE_MV 4300  //Battery is put into emergency stop if one cell goes over this value
 #define MIN_CELL_VOLTAGE_MV 2800  //Battery is put into emergency stop if one cell goes below this value
 #define MAX_CHARGE_POWER_ALLOWED_W 5000
-#define MAX_CHARGE_POWER_WHEN_TOPBALANCING_W 500 
+#define MAX_CHARGE_POWER_WHEN_TOPBALANCING_W 500
 #define RAMPDOWN_SOC 9000  // (90.00) SOC% to start ramping down from max charge power towards 0 at 100.00%
-#define STALE_PERIOD_CONFIG 180000; //Number of milliseconds before critical values are classed as stale/stuck 180000 = 180 seconds
+#define STALE_PERIOD_CONFIG \
+  180000;  //Number of milliseconds before critical values are classed as stale/stuck 180000 = 180 seconds
 void setup_battery(void);
 void transmit_can(CAN_frame* tx_frame, int interface);
 
