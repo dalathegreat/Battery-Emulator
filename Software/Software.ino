faf22e6a4c01b20cd7726018095a90879ab9d834
Daniel Öster <dalathegreat@gmail.com>
2024-11-25 23:05:34 +0200

    Update SMA Tripower based on logs

--- Software/Software.ino
+++ Software/Software.ino
@@ -545,6 +545,10 @@ void init_contactors() {
   pinMode(BMS_POWER, OUTPUT);
   digitalWrite(BMS_POWER, HIGH);
 #endif  //HW_STARK
+#ifdef SMA_TRIPOWER_CAN
+  //pinMode(INVERTER_CONTACTOR_ENABLE_PIN, OUTPUT); //Incase this is an output
+  //digitalWrite(INVERTER_CONTACTOR_ENABLE_PIN, LOW); //Incase this is an output
+#endif  //SMA_TRIPOWER_CAN
 }
 
 void init_rs485() {
@@ -741,6 +745,11 @@ void check_interconnect_available() {
 void handle_contactors() {
 #ifdef BYD_SMA
   datalayer.system.status.inverter_allows_contactor_closing = digitalRead(INVERTER_CONTACTOR_ENABLE_PIN);
+#endif
+#ifdef SMA_TRIPOWER_CAN
+  //set(INVERTER_CONTACTOR_ENABLE_PIN, ON); //TODO: is this an input, or an output? Figure out and make logic
+  datalayer.system.status.inverter_allows_contactor_closing =
+      digitalRead(INVERTER_CONTACTOR_ENABLE_PIN);  //Incase it is input
 #endif
 
 #ifdef CONTACTOR_CONTROL_DOUBLE_BATTERY
