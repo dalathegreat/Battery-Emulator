f3d24cd9768f54443992ab436f49449c77a08cc8
mvgalen <marijnvangalen@gmail.com>
2024-11-30 15:55:34 +0100

    Add missing @endif

--- Software/src/devboard/webserver/advanced_battery_html.cpp
+++ Software/src/devboard/webserver/advanced_battery_html.cpp
@@ -338,6 +338,7 @@ String advanced_battery_processor(const String& var) {
     content += "<h4>SolvedChallenge: " + String(datalayer_extended.nissanleaf.SolvedChallengeMSB) +
                String(datalayer_extended.nissanleaf.SolvedChallengeLSB) + "</h4>";
     content += "<h4>Challenge failed: " + String(datalayer_extended.nissanleaf.challengeFailed) + "</h4>";#endif
+#endif
 
 #ifdef MEB_BATTERY
     content += datalayer_extended.meb.SDSW ? "<h4>Service disconnect switch: Missing!</h4>"
