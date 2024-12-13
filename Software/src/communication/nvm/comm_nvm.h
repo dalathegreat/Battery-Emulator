4c4b4eef81c14a0335c2ed4a63c4ba9bb0e67018
lenvm <lenvm@users.noreply.github.com>
2024-12-13 17:02:34 +0100

    move setting storage related settings from Software.ino to nvme folder

---
+++ Software/src/communication/nvm/comm_nvm.h
@@ -0,0 +1,16 @@
+#ifndef _COMM_NVM_H_
+#define _COMM_NVM_H_
+
+#include "../../include.h"
+
+#include "../../datalayer/datalayer.h"
+#include "../../devboard/utils/events.h"
+#include "../../devboard/wifi/wifi.h"
+
+void init_stored_settings();
+
+void store_settings_equipment_stop();
+
+void storeSettings();
+
+#endif
