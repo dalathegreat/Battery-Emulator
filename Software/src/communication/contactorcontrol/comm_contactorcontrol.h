624926b552a5ab31cd06cd676af3f62a65cce252
lenvm <lenvm@users.noreply.github.com>
2024-12-13 16:35:42 +0100

    move contactor control related functions from Software.ino to comm_contactorcontrol folder

---
+++ Software/src/communication/contactorcontrol/comm_contactorcontrol.h
@@ -0,0 +1,15 @@
+#ifndef _COMM_CONTACTORCONTROL_H_
+#define _COMM_CONTACTORCONTROL_H_
+
+#include "../../include.h"
+
+#include "../../datalayer/datalayer.h"
+#include "../../devboard/utils/events.h"
+
+void init_contactors();
+
+void handle_contactors();
+
+void handle_contactors_battery2();
+
+#endif
