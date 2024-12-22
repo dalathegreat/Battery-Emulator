b4474e504d38d65f2fc01affcf8599b1cbab0799
No-Signal <matt@holmes.world>
2024-12-22 10:46:54 +0000

    Moving additional sensitive settings to USER_SECRETS.h

--- Software/USER_SECRETS.TEMPLATE.h
+++ Software/USER_SECRETS.TEMPLATE.h
@@ -1,8 +1,13 @@
 #define WIFI_SSID "REPLACE_WITH_YOUR_SSID"          // Maximum of 63 characters
 #define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"  // Minimum of 8 characters
-#define AP_PASSWORD "123456789"        // Minimum of 8 characters; set to blank if you want the access point to be open
-#define HTTP_USERNAME "admin"          // username to webserver authentication;
-#define HTTP_PASSWORD "admin"          // password to webserver authentication;
+#define AP_PASSWORD "123456789"  // Minimum of 8 characters; set to blank if you want the access point to be open
+
+#define WEBSERVER_AUTH_REQUIRED \
+  false  //Set this line to true to activate webserver authentication (this line must not be commented).
+#define HTTP_USERNAME "admin"  // username to webserver authentication;
+#define HTTP_PASSWORD "admin"  // password to webserver authentication;
+
 #define MQTT_SERVER "192.168.xxx.yyy"  // mqtt server address
+#define MQTT_PORT 1883                 // mqtt server port
 #define MQTT_USER NULL                 // mqtt username, leave blank for no authentication
 #define MQTT_PASSWORD NULL             // mqtt password, leave blank for no authentication
