e35be65145d3ab727585db1cf393fba4a97f1347
mvgalen <marijnvangalen@gmail.com>
2025-01-02 11:44:22 +0100

    Fix issue with % in log output conflicting with templating character
    - Split index_html string in header and footer part
    - Use header and footer for index html template page
    - Fix missing html and head tags in minified index html.
    - Do not use templating engine for debug and can log pages, by using index html header and footer directly in generated content.

--- Software/src/devboard/webserver/index_html.cpp
+++ Software/src/devboard/webserver/index_html.cpp
@@ -1,6 +1,11 @@
-const char index_html[] = R"rawliteral(
-<!doctypehtml><title>Battery Emulator</title><meta content="width=device-width"name=viewport><style>html{font-family:Arial;display:inline-block;text-align:center}h2{font-size:3rem}body{max-width:800px;margin:0 auto}</style>%X%
-)rawliteral";
+#include "index_html.h"
+
+#define INDEX_HTML_HEADER R"rawliteral(<!doctype html><html><head><title>Battery Emulator</title><meta content="width=device-width"name=viewport><style>html{font-family:Arial;display:inline-block;text-align:center}h2{font-size:3rem}body{max-width:800px;margin:0 auto}</style><body>)rawliteral"
+#define INDEX_HTML_FOOTER R"rawliteral(</body></html>)rawliteral";
+
+const char index_html[] = INDEX_HTML_HEADER "%X%" INDEX_HTML_FOOTER;
+const char index_html_header[] = INDEX_HTML_HEADER;
+const char index_html_footer[] = INDEX_HTML_FOOTER;
 
 /* The above code is minified (https://kangax.github.io/html-minifier/) to increase performance. Here is the full HTML function:
 <!DOCTYPE HTML><html>
