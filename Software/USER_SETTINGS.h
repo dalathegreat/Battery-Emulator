03b50c0ca529dc56e27e4537b93f0a1ad2a6bab1
laughingguffly <eddy.blum+nospam@gmail.com>
2025-02-10 21:39:42 +0000

    rebase from main

--- Software/USER_SETTINGS.h
+++ Software/USER_SETTINGS.h
@@ -76,7 +76,8 @@
 //#define NC_CONTACTORS         //Enable this line to control normally closed contactors. CONTACTOR_CONTROL must be enabled for this option. Extremely rare setting!
 //#define PERIODIC_BMS_RESET    //Enable to have the emulator powercycle the connected battery every 24hours via GPIO. Useful for some batteries like Nissan LEAF
 //#define REMOTE_BMS_RESET      //Enable to allow the emulator to remotely trigger a powercycle of the battery via MQTT. Useful for some batteries like Nissan LEAF
-// PERIODIC_BMS_RESET_AT Uses NTP server, internet required. In 24 Hour format WITHOUT leading 0. e.g 0230 should be 230. Time Zone is set in USER_SETTINGS.cpp
+//#define REMOTE_BMS_RESET      //Enable to allow the emulator to remotely trigger a powercycle of the battery via MQTT. Useful for some batteries like Nissan LEAF
+// PERIODIC_BMS_RESET_AT In 24 Hour format WITHOUT leading 0. e.g 0230 should be 230. Time Zone is set in USER_SETTINGS.cpp
 #define PERIODIC_BMS_RESET_AT 525
 
 /* Shunt/Contactor settings (Optional) */
