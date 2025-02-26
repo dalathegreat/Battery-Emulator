69154e26d50212562e3d145269992539dacd9dce
Jamie Jones <jamie@jamie-jones.co.uk>
2025-02-26 15:54:18 +0000

    Running pre-commit

--- Software/src/battery/SIMPBMS-BATTERY.cpp
+++ Software/src/battery/SIMPBMS-BATTERY.cpp
@@ -31,7 +31,6 @@ static uint8_t charge_forbidden = 0;
 static uint8_t discharge_forbidden = 0;
 static uint16_t cellvoltages_mV[SIMPBMS_MAX_CELLS] = {0};
 
-
 void update_values_battery() {
 
   datalayer.battery.status.real_soc = (SOC * 100);  //increase SOC range from 0-100 -> 100.00
@@ -80,13 +79,13 @@ void handle_incoming_can_frame_battery(CAN_frame rx_frame) {
       //discharge and charge limits
       charge_cutoff_voltage = ((rx_frame.data.u8[1] << 8) | rx_frame.data.u8[0]);
       discharge_cutoff_voltage = ((rx_frame.data.u8[7] << 8) | rx_frame.data.u8[6]);
-      max_charge_current = (((rx_frame.data.u8[3] << 8) | rx_frame.data.u8[2])) /10 ; 
+      max_charge_current = (((rx_frame.data.u8[3] << 8) | rx_frame.data.u8[2])) / 10;
       max_discharge_current = (((rx_frame.data.u8[5] << 8) | rx_frame.data.u8[4])) / 10;
       break;
     case 0x356:
       //current status
-      current_dA = ((rx_frame.data.u8[3]<< 8) + rx_frame.data.u8[2]) / 1000;
-      voltage_dV = ((rx_frame.data.u8[1]<< 8) + rx_frame.data.u8[0]) / 10 ;
+      current_dA = ((rx_frame.data.u8[3] << 8) + rx_frame.data.u8[2]) / 1000;
+      voltage_dV = ((rx_frame.data.u8[1] << 8) + rx_frame.data.u8[0]) / 10;
       break;
     case 0x373:
       //min/max values
@@ -97,12 +96,12 @@ void handle_incoming_can_frame_battery(CAN_frame rx_frame) {
       break;
     case 0x372:
       //cells_in_series = rx_frame.data.u8[0];
-      if (rx_frame.data.u8[3]  > 0 && rx_frame.data.u8[3] <= SIMPBMS_MAX_CELLS) {
+      if (rx_frame.data.u8[3] > 0 && rx_frame.data.u8[3] <= SIMPBMS_MAX_CELLS) {
         uint8_t cellnumber = rx_frame.data.u8[3];
         if (cellnumber > cells_in_series) {
           cells_in_series = cellnumber;
         }
-        cellvoltages_mV[cellnumber - 1] =  ((rx_frame.data.u8[7] << 8) | rx_frame.data.u8[6]);
+        cellvoltages_mV[cellnumber - 1] = ((rx_frame.data.u8[7] << 8) | rx_frame.data.u8[6]);
       }
 
       break;
@@ -110,15 +109,13 @@ void handle_incoming_can_frame_battery(CAN_frame rx_frame) {
       ah_total = ((rx_frame.data.u8[1] << 8) | rx_frame.data.u8[0]);
 
       break;
-   
 
     default:
       break;
   }
 }
 
-void transmit_can_battery() {
-}
+void transmit_can_battery() {}
 
 void setup_battery(void) {  // Performs one time setup at startup
   strncpy(datalayer.system.info.battery_protocol, "SIMPBMS battery", 63);
@@ -129,7 +126,6 @@ void setup_battery(void) {  // Performs one time setup at startup
   datalayer.battery.info.max_cell_voltage_mV = MAX_CELL_VOLTAGE_MV;
   datalayer.battery.info.min_cell_voltage_mV = MIN_CELL_VOLTAGE_MV;
   datalayer.system.status.battery_allows_contactor_closing = true;
-
 }
 
 #endif
