84fb5bab158c51b199c7063ddf86a53ceac8993e
Daniel Öster <dalathegreat@gmail.com>
2025-03-03 23:28:05 +0200

    Add support for 96S 69kWh SPA battery

--- Software/src/battery/VOLVO-SPA-BATTERY.h
+++ Software/src/battery/VOLVO-SPA-BATTERY.h
@@ -4,8 +4,10 @@
 #include "../include.h"
 
 #define BATTERY_SELECTED
-#define MAX_PACK_VOLTAGE_DV 4540  //5000 = 500.0V
-#define MIN_PACK_VOLTAGE_DV 2938
+#define MAX_PACK_VOLTAGE_108S_DV 4540
+#define MIN_PACK_VOLTAGE_108S_DV 2938
+#define MAX_PACK_VOLTAGE_96S_DV 4030
+#define MIN_PACK_VOLTAGE_96S_DV 2620
 #define MAX_CELL_DEVIATION_MV 250
 #define MAX_CELL_VOLTAGE_MV 4210  //Battery is put into emergency stop if one cell goes over this value
 #define MIN_CELL_VOLTAGE_MV 2700  //Battery is put into emergency stop if one cell goes below this value
