545fceb4ccb5ea3f2769fc6d1af014452140e4ce
Daniel Öster <dalathegreat@gmail.com>
2025-03-05 23:04:56 +0200

    Update AsyncTCP to v3.3.6

--- Software/src/lib/ESP32Async-AsyncTCP/src/AsyncTCPVersion.h
+++ Software/src/lib/ESP32Async-AsyncTCP/src/AsyncTCPVersion.h
@@ -12,7 +12,7 @@ extern "C" {
 /** Minor version number (x.X.x) */
 #define ASYNCTCP_VERSION_MINOR 3
 /** Patch version number (x.x.X) */
-#define ASYNCTCP_VERSION_PATCH 5
+#define ASYNCTCP_VERSION_PATCH 6
 
 /**
  * Macro to convert version number into an integer
