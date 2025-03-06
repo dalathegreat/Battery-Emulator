476c54711fc1a0e305d67559082ce936dc108091
Daniel Öster <dalathegreat@gmail.com>
2025-03-06 19:54:13 +0200

    Change return type of function

--- Software/src/lib/miwagner-ESP32-Arduino-CAN/CAN.c
+++ Software/src/lib/miwagner-ESP32-Arduino-CAN/CAN.c
@@ -264,7 +264,7 @@ int CAN_init() {
 	return 0;
 }
 
-int CAN_write_frame(const CAN_frame_t *p_frame) {
+bool CAN_write_frame(const CAN_frame_t *p_frame) {
 	if (sem_tx_complete == NULL) {
 		return 0;
 	}
