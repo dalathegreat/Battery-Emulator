9a7b472462888f13b6dc5136951d93727d18676a
Daniel Öster <dalathegreat@gmail.com>
2025-03-16 12:33:53 +0200

    Update Software.ino

--- Software/Software.ino
+++ Software/Software.ino
@@ -49,7 +49,7 @@
 volatile unsigned long long bmsResetTimeOffset = 0;
 
 // The current software version, shown on webserver
-const char* version_number = "8.9.dev";
+const char* version_number = "8.9.0";
 
 // Interval settings
 uint16_t intervalUpdateValues = INTERVAL_1_S;  // Interval at which to update inverter values / Modbus registers
