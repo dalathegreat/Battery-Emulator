d934a3b796531343f717fd3a4167f0ff46bf973c
Jaakko Haakana <jaakko@haakana.net>
2025-05-06 21:33:33 +0300

    BYD CAN inverter protocol class

--- Software/src/inverter/BYD-CAN.h
+++ Software/src/inverter/BYD-CAN.h
@@ -3,11 +3,101 @@
 #include "../include.h"
 
 #define CAN_INVERTER_SELECTED
+#define SELECTED_INVERTER_CLASS BydCanInverter
+
 #define FW_MAJOR_VERSION 0x03
 #define FW_MINOR_VERSION 0x29
 
-void send_intial_data();
-void transmit_can_frame(CAN_frame* tx_frame, int interface);
-void setup_inverter(void);
+class BydCanInverter : public CanInverterProtocol {
+ public:
+  void setup();
+  void send_intial_data();
+  void transmit_can(unsigned long currentMillis);
+  void map_can_frame_to_variable(CAN_frame rx_frame);
+
+ private:
+  void update_values();
+
+  unsigned long previousMillis2s = 0;   // will store last time a 2s CAN Message was send
+  unsigned long previousMillis10s = 0;  // will store last time a 10s CAN Message was send
+  unsigned long previousMillis60s = 0;  // will store last time a 60s CAN Message was send
+
+#define VOLTAGE_OFFSET_DV 20
+
+  CAN_frame BYD_250 = {.FD = false,
+                       .ext_ID = false,
+                       .DLC = 8,
+                       .ID = 0x250,
+                       .data = {FW_MAJOR_VERSION, FW_MINOR_VERSION, 0x00, 0x66, (uint8_t)((BATTERY_WH_MAX / 100) >> 8),
+                                (uint8_t)(BATTERY_WH_MAX / 100), 0x02,
+                                0x09}};  //0-1 FW version , Capacity kWh byte4&5 (example 24kWh = 240)
+  CAN_frame BYD_290 = {.FD = false,
+                       .ext_ID = false,
+                       .DLC = 8,
+                       .ID = 0x290,
+                       .data = {0x06, 0x37, 0x10, 0xD9, 0x00, 0x00, 0x00, 0x00}};
+  CAN_frame BYD_2D0 = {.FD = false,
+                       .ext_ID = false,
+                       .DLC = 8,
+                       .ID = 0x2D0,
+                       .data = {0x00, 0x42, 0x59, 0x44, 0x00, 0x00, 0x00, 0x00}};  //BYD
+  CAN_frame BYD_3D0_0 = {.FD = false,
+                         .ext_ID = false,
+                         .DLC = 8,
+                         .ID = 0x3D0,
+                         .data = {0x00, 0x42, 0x61, 0x74, 0x74, 0x65, 0x72, 0x79}};  //Battery
+  CAN_frame BYD_3D0_1 = {.FD = false,
+                         .ext_ID = false,
+                         .DLC = 8,
+                         .ID = 0x3D0,
+                         .data = {0x01, 0x2D, 0x42, 0x6F, 0x78, 0x20, 0x50, 0x72}};  //-Box Pr
+  CAN_frame BYD_3D0_2 = {.FD = false,
+                         .ext_ID = false,
+                         .DLC = 8,
+                         .ID = 0x3D0,
+                         .data = {0x02, 0x65, 0x6D, 0x69, 0x75, 0x6D, 0x20, 0x48}};  //emium H
+  CAN_frame BYD_3D0_3 = {.FD = false,
+                         .ext_ID = false,
+                         .DLC = 8,
+                         .ID = 0x3D0,
+                         .data = {0x03, 0x56, 0x53, 0x00, 0x00, 0x00, 0x00, 0x00}};  //VS
+  //Actual content messages
+  CAN_frame BYD_110 = {.FD = false,
+                       .ext_ID = false,
+                       .DLC = 8,
+                       .ID = 0x110,
+                       .data = {0x01, 0x90, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}};
+  CAN_frame BYD_150 = {.FD = false,
+                       .ext_ID = false,
+                       .DLC = 8,
+                       .ID = 0x150,
+                       .data = {0x00, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00}};
+  CAN_frame BYD_190 = {.FD = false,
+                       .ext_ID = false,
+                       .DLC = 8,
+                       .ID = 0x190,
+                       .data = {0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00}};
+  CAN_frame BYD_1D0 = {.FD = false,
+                       .ext_ID = false,
+                       .DLC = 8,
+                       .ID = 0x1D0,
+                       .data = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x08}};
+  CAN_frame BYD_210 = {.FD = false,
+                       .ext_ID = false,
+                       .DLC = 8,
+                       .ID = 0x210,
+                       .data = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}};
+
+  int16_t temperature_average = 0;
+  uint16_t inverter_voltage = 0;
+  uint16_t inverter_SOC = 0;
+  int16_t inverter_current = 0;
+  int16_t inverter_temperature = 0;
+  uint16_t remaining_capacity_ah = 0;
+  uint16_t fully_charged_capacity_ah = 0;
+  long inverter_timestamp = 0;
+  bool initialDataSent = false;
+  bool inverterStartedUp = false;
+};
 
 #endif
--- Software/src/inverter/BYD-MODBUS.h
+++ Software/src/inverter/BYD-MODBUS.h
@@ -3,7 +3,7 @@
 #include "../include.h"
 
 #define MODBUS_INVERTER_SELECTED
-#define OO_INVERTER_PROTOCOL_SELECTED
+#define SELECTED_INVERTER_CLASS BydModbusInverter
 
 #define MB_RTU_NUM_VALUES 13100
 #define MAX_POWER 40960  //BYD Modbus specific value
