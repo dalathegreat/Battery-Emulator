36c282739aad1224cb24039b7b90e0840909c001
Matt Holmes <matt@holmes.world>
2025-05-10 21:38:53 +0100

    Fixes for Chevy Volt Charger debug logging

--- Software/src/charger/CHEVY-VOLT-CHARGER.cpp
+++ Software/src/charger/CHEVY-VOLT-CHARGER.cpp
@@ -71,7 +71,7 @@ void ChevyVoltCharger::map_can_frame_to_variable(CAN_frame rx_frame) {
       break;
     default:
 #ifdef DEBUG_LOG
-      logging.printf("CAN Rcv unknown frame MsgID=%x\n", rx_frame.MsgID);
+      logging.printf("CAN Rcv unknown frame MsgID=%x\n", rx_frame.ID);
 #endif
       break;
   }
@@ -149,9 +149,9 @@ void ChevyVoltCharger::transmit_can(unsigned long currentMillis) {
   /* Serial echo every 5s of charger stats */
   if (currentMillis - previousMillis5000ms >= INTERVAL_5_S) {
     previousMillis5000ms = currentMillis;
-    logging.printf("Charger AC in IAC=%fA VAC=%fV\n", charger_stat_ACcur, charger_stat_ACvol);
-    logging.printf("Charger HV out IDC=%fA VDC=%fV\n", charger_stat_HVcur, charger_stat_HVvol);
-    logging.printf("Charger LV out IDC=%fA VDC=%fV\n", charger_stat_LVcur, charger_stat_LVvol);
+    logging.printf("Charger AC in IAC=%fA VAC=%fV\n", datalayer.charger.charger_stat_ACcur, datalayer.charger.charger_stat_ACvol);
+    logging.printf("Charger HV out IDC=%fA VDC=%fV\n", datalayer.charger.charger_stat_HVcur, datalayer.charger.charger_stat_HVvol);
+    logging.printf("Charger LV out IDC=%fA VDC=%fV\n", datalayer.charger.charger_stat_LVcur, datalayer.charger.charger_stat_LVvol);
     logging.printf("Charger mode=%s\n", (charger_mode > MODE_DISABLED) ? "Enabled" : "Disabled");
     logging.printf("Charger HVset=%uV,%uA finishCurrent=%uA\n", setpoint_HV_VDC, setpoint_HV_IDC, setpoint_HV_IDC_END);
   }
