e153be1c77282dc9022c4ab17271a5fa2d406dca
Daniel Öster <dalathegreat@gmail.com>
2025-05-20 23:49:35 +0300

    Move cellvoltage limits to private class

--- Software/src/battery/GEELY-GEOMETRY-C-BATTERY.h
+++ Software/src/battery/GEELY-GEOMETRY-C-BATTERY.h
@@ -8,14 +8,6 @@
 #define BATTERY_SELECTED
 #define SELECTED_BATTERY_CLASS GeelyGeometryCBattery
 
-#define MAX_PACK_VOLTAGE_70_DV 4420  //70kWh
-#define MIN_PACK_VOLTAGE_70_DV 2860
-#define MAX_PACK_VOLTAGE_53_DV 4160  //53kWh
-#define MIN_PACK_VOLTAGE_53_DV 2700
-#define MAX_CELL_DEVIATION_MV 150
-#define MAX_CELL_VOLTAGE_MV 4250  //Battery is put into emergency stop if one cell goes over this value
-#define MIN_CELL_VOLTAGE_MV 2700  //Battery is put into emergency stop if one cell goes below this value
-
 #define POLL_SOC 0x4B35
 #define POLL_CC2_VOLTAGE 0x4BCF
 #define POLL_CELL_MAX_VOLTAGE_NUMBER 0x4B1E
@@ -45,7 +37,12 @@ class GeelyGeometryCBattery : public CanBattery {
   virtual void transmit_can(unsigned long currentMillis);
 
  private:
-  DATALAYER_BATTERY_TYPE* datalayer_battery;
+  const int MAX_PACK_VOLTAGE_70_DV 4420                                            //70kWh
+      const int MIN_PACK_VOLTAGE_70_DV 2860 const int MAX_PACK_VOLTAGE_53_DV 4160  //53kWh
+      const int MIN_PACK_VOLTAGE_53_DV 2700 const int MAX_CELL_DEVIATION_MV 150 const int
+          MAX_CELL_VOLTAGE_MV 4250        //Battery is put into emergency stop if one cell goes over this value
+      const int MIN_CELL_VOLTAGE_MV 2700  //Battery is put into emergency stop if one cell goes below this value
+      DATALAYER_BATTERY_TYPE* datalayer_battery;
   DATALAYER_INFO_GEELY_GEOMETRY_C* datalayer_geometryc;
 
   CAN_frame GEELY_191 = {.FD = false,  //PAS_APA_Status , 10ms
