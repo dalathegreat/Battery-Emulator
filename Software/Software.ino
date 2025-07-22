b6b812690fd2fe7d8c916a7883e0f4635d143c28
Daniel Öster <dalathegreat@gmail.com>
2025-07-22 11:33:54 +0300

    Update Software.ino

--- Software/Software.ino
+++ Software/Software.ino
@@ -34,7 +34,7 @@
 volatile unsigned long long bmsResetTimeOffset = 0;
 
 // The current software version, shown on webserver
-const char* version_number = "8.16.0";
+const char* version_number = "9.0.experimental";
 
 // Interval timers
 volatile unsigned long currentMillis = 0;
