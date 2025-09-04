c1f318730654f7de67c2e14edadcade27c035c94
Daniel Öster <dalathegreat@gmail.com>
2025-09-04 20:36:12 +0300

    Update Software.cpp

--- Software/Software.cpp
+++ Software/Software.cpp
@@ -34,7 +34,7 @@
 #endif
 
 // The current software version, shown on webserver
-const char* version_number = "9.0.RC6experimental";
+const char* version_number = "9.0.RC6";
 
 // Interval timers
 volatile unsigned long currentMillis = 0;
