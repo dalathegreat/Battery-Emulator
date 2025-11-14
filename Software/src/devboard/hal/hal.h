6127d40f2b68840a1c830c73f07c5432b4e1cfcf
Jonny <jonny@jonnybarker.com>
2025-11-14 16:23:12 +0000

    Add support for i2c SSD1306 128x64 displays

--- Software/src/devboard/hal/hal.h
+++ Software/src/devboard/hal/hal.h
@@ -155,6 +155,10 @@ class Esp32Hal {
   virtual gpio_num_t LED_PIN() { return GPIO_NUM_NC; }
   virtual uint8_t LED_MAX_BRIGHTNESS() { return 40; }
 
+  // i2c display
+  virtual gpio_num_t DISPLAY_SDA_PIN() { return GPIO_NUM_NC; }
+  virtual gpio_num_t DISPLAY_SCL_PIN() { return GPIO_NUM_NC; }
+
   // Equipment stop pin
   virtual gpio_num_t EQUIPMENT_STOP_PIN() { return GPIO_NUM_NC; }
 
