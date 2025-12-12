fcb1c31d1322165819a046a432ec472871e87a1a
Daniel Öster <dalathegreat@gmail.com>
2025-12-12 13:57:28 +0200

    Remove inverter opening, not reliable since it locks over 8s

--- Software/src/battery/THINK-BATTERY.cpp
+++ Software/src/battery/THINK-BATTERY.cpp
@@ -122,7 +122,7 @@ void ThinkBattery::transmit_can(unsigned long currentMillis) {
   if (currentMillis - previousMillis200 >= INTERVAL_200_MS) {
     previousMillis200 = currentMillis;
 
-    if (datalayer.system.status.inverter_allows_contactor_closing && (datalayer.battery.status.bms_status != FAULT)) {
+    if (datalayer.battery.status.bms_status != FAULT) {
       transmit_can_frame(&PCU_310);
       transmit_can_frame(&PCU_311);
     }
