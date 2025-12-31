f430a8bbe3ad0e503b8e77ace140f1239b4938bd
Jonny <jonny@jonnybarker.com>
2025-12-31 09:38:03 +0000

    MG4: Tweak contactor-close cycle timing

--- Software/src/battery/MG-4-BATTERY.h
+++ Software/src/battery/MG-4-BATTERY.h
@@ -15,15 +15,17 @@ class Mg4Battery : public UdsCanBattery {
  private:
   static const uint16_t MAX_CELL_DEVIATION_MV = 150;
 
+  int sendPhase = 0;
+  unsigned long nextMillis = 0;
+
   unsigned long previousMillis10 = 0;   // will store last time a 10ms CAN Message was send
   unsigned long previousMillis200 = 0;  // will store last time a 200ms CAN Message was send
-  unsigned long four_seven_counter = 0;
 
   CAN_frame MG4_4F3 = {.FD = false,
                        .ext_ID = false,
                        .DLC = 8,
                        .ID = 0x4F3,
-                       .data = {0xF3, 0x10, 0x48, 0x00, 0xFF, 0xFF, 0x00, 0x10}};
+                       .data = {0xF3, 0x10, 0x44, 0x00, 0xFF, 0xFF, 0x00, 0x11}};
   CAN_frame MG4_047_E9 = {.FD = false,
                           .ext_ID = false,
                           .DLC = 8,
