258385308a8ddc795f692d46d0a83e4307960881
Jonny <jonny@jonnybarker.com>
2026-01-01 13:52:13 +0000

    MG4: Attempt to mimic a 33 frame 047/4F3 cycle

--- Software/src/battery/MG-4-BATTERY.h
+++ Software/src/battery/MG-4-BATTERY.h
@@ -16,7 +16,6 @@ class Mg4Battery : public UdsCanBattery {
   static const uint16_t MAX_CELL_DEVIATION_MV = 150;
 
   int sendPhase = 0;
-  unsigned long nextMillis = 0;
 
   unsigned long previousMillis10 = 0;   // will store last time a 10ms CAN Message was send
   unsigned long previousMillis200 = 0;  // will store last time a 200ms CAN Message was send
@@ -25,17 +24,12 @@ class Mg4Battery : public UdsCanBattery {
                        .ext_ID = false,
                        .DLC = 8,
                        .ID = 0x4F3,
-                       .data = {0xF3, 0x10, 0x44, 0x00, 0xFF, 0xFF, 0x00, 0x11}};
-  CAN_frame MG4_047_E9 = {.FD = false,
-                          .ext_ID = false,
-                          .DLC = 8,
-                          .ID = 0x047,
-                          .data = {0xE9, 0x0A, 0x45, 0x7D, 0x7F, 0xFF, 0xFF, 0xFE}};
-  CAN_frame MG4_047_3B = {.FD = false,
-                          .ext_ID = false,
-                          .DLC = 8,
-                          .ID = 0x047,
-                          .data = {0x3B, 0x0B, 0x45, 0x7D, 0x7F, 0xFF, 0xFF, 0xFE}};
+                       .data = {0xF3, 0x10, 0x48, 0x00, 0xFF, 0xFF, 0x00, 0x11}};
+  CAN_frame MG4_047 = {.FD = false,
+                       .ext_ID = false,
+                       .DLC = 8,
+                       .ID = 0x047,
+                       .data = {0x00, 0x00, 0x45, 0x7D, 0x7F, 0xFF, 0xFF, 0xFE}};
 };
 
 #endif
