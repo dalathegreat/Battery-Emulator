b7de16c0de085b40aaea63b25108c394fe3a7d27
lenvm <lenvm@users.noreply.github.com>
2026-01-03 00:46:17 +0100

    SMA BYD-H and BYD-HVS: update how bms_status is sent to inverter
    - send STOP_STATE only when bms_status == FAULT, just like SMA-TRIPOWER, rather than when the bms_status is not ACTIVE
    - this should prevent the inverter going into error whenever the bms_status is not ACTIVE

--- Software/src/inverter/SMA-BYD-HVS-CAN.cpp
+++ Software/src/inverter/SMA-BYD-HVS-CAN.cpp
@@ -54,10 +54,10 @@ void SmaBydHvsInverter::
   SMA_4D8.data.u8[4] = (temperature_average >> 8);
   SMA_4D8.data.u8[5] = (temperature_average & 0x00FF);
   //Battery ready
-  if (datalayer.battery.status.bms_status == ACTIVE) {
-    SMA_4D8.data.u8[6] = READY_STATE;
-  } else {
+  if (datalayer.battery.status.bms_status == FAULT) {
     SMA_4D8.data.u8[6] = STOP_STATE;
+  } else {
+    SMA_4D8.data.u8[6] = READY_STATE;
   }
 
   //Highest battery temperature
