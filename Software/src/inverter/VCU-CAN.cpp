13abdc4cbaf0afe9fd730194d7039ea316b0fc1d
Daniel Öster <dalathegreat@gmail.com>
2026-01-04 20:24:29 +0200

    Add all LEAF CAN messages to sending routine

--- Software/src/inverter/VCU-CAN.cpp
+++ Software/src/inverter/VCU-CAN.cpp
@@ -1,13 +1,45 @@
 #include "VCU-CAN.h"
 #include "../communication/can/comm_can.h"
 #include "../datalayer/datalayer.h"
+#include "../devboard/utils/common_functions.h"  //For CRC table
 #include "../inverter/INVERTERS.h"
 
-void VCUInverter::update_values() {}
+static uint8_t calculate_CRC_Nissan(CAN_frame* frame) {
+  uint8_t crc = 0;
+  for (uint8_t j = 0; j < 7; j++) {
+    crc = crctable_nissan_leaf[(crc ^ static_cast<uint8_t>(frame->data.u8[j])) % 256];
+  }
+  return crc;
+}
+
+void VCUInverter::update_values() {  //Called every 1s
+
+  LEAF_1DC.data.u8[0] = ((datalayer.battery.status.max_discharge_power_W / 4) << 2);
+  LEAF_1DC.data.u8[1] = ((datalayer.battery.status.max_discharge_power_W / 4) << 6);
+  LEAF_1DC.data.u8[1] = (LEAF_1DC.data.u8[1] || (((datalayer.battery.status.max_charge_power_W / 4) >> 4) & 0x3F));
+  LEAF_1DC.data.u8[2] = ((datalayer.battery.status.max_charge_power_W / 4) << 4);
+
+  LEAF_55B.data.u8[0] = ((datalayer.battery.status.real_soc / 10) << 2);
+  LEAF_55B.data.u8[1] = ((datalayer.battery.status.real_soc / 10) << 6);
+
+  remining_gids = (datalayer.battery.status.real_soc / 10000.0) * 281;  //0-281 for 24kWh
+  LEAF_5BC.data.u8[0] = remining_gids << 2;
+  LEAF_5BC.data.u8[1] = remining_gids << 6;
+  LEAF_5BC.data.u8[4] = (datalayer.battery.status.soh_pptt / 100) << 1;
+}
 
 void VCUInverter::map_can_frame_to_variable(CAN_frame rx_frame) {
   switch (rx_frame.ID) {
-    case 0x1A:
+    case 0x1F2:
+      datalayer.system.status.CAN_inverter_still_alive = CAN_STILL_ALIVE;
+      break;
+    case 0x1D4:
+      datalayer.system.status.CAN_inverter_still_alive = CAN_STILL_ALIVE;
+      break;
+    case 0x50B:
+      datalayer.system.status.CAN_inverter_still_alive = CAN_STILL_ALIVE;
+      break;
+    case 0x50C:
       datalayer.system.status.CAN_inverter_still_alive = CAN_STILL_ALIVE;
       break;
     default:
@@ -20,13 +52,27 @@ void VCUInverter::transmit_can(unsigned long currentMillis) {
   if (currentMillis - previousMillis10ms >= INTERVAL_10_MS) {
     previousMillis10ms = currentMillis;
 
+    mprun10 = (mprun10 + 1) % 4;  // mprun10 cycles between 0-1-2-3-0-1...
+    LEAF_1DC.data.u8[6] = mprun10;
+    LEAF_1DC.data.u8[7] = calculate_CRC_Nissan(&LEAF_1DC);
     transmit_can_frame(&LEAF_1DC);
+    LEAF_1DB.data.u8[6] = mprun10;
+    LEAF_1DB.data.u8[7] = calculate_CRC_Nissan(&LEAF_1DB);
     transmit_can_frame(&LEAF_1DB);
   }
   // Send 100ms CAN Messages
   if (currentMillis - previousMillis100ms >= INTERVAL_100_MS) {
     previousMillis100ms = currentMillis;
 
+    mprun100 = (mprun100 + 1) % 4;  // mprun10 cycles between 0-1-2-3-0-1...
+    counter_55B = (counter_55B + 1) % 10;
+    if (counter_55B < 5) {
+      LEAF_55B.data.u8[2] = 0xAA;
+    } else {
+      LEAF_55B.data.u8[2] = 0x55;
+    }
+    LEAF_55B.data.u8[6] = (1 << 4) | mprun100;  //1 = RefuseToSleep 2 = ReadyToSleep
+    LEAF_55B.data.u8[7] = calculate_CRC_Nissan(&LEAF_55B);
     transmit_can_frame(&LEAF_55B);
     transmit_can_frame(&LEAF_5BC);
   }
@@ -35,6 +81,13 @@ void VCUInverter::transmit_can(unsigned long currentMillis) {
     previousMillis500ms = currentMillis;
 
     transmit_can_frame(&LEAF_59E);
+    if (LEAF_5C0.data.u8[0] == 0x40) {
+      LEAF_5C0.data.u8[0] = 0x80;
+    } else if (LEAF_5C0.data.u8[0] == 0x80) {
+      LEAF_5C0.data.u8[0] = 0xC0;
+    } else if (LEAF_5C0.data.u8[0] == 0xC0) {
+      LEAF_5C0.data.u8[0] = 0x40;
+    }
     transmit_can_frame(&LEAF_5C0);
   }
 }
--- Software/src/inverter/VCU-CAN.h
+++ Software/src/inverter/VCU-CAN.h
@@ -15,11 +15,15 @@ class VCUInverter : public CanInverterProtocol {
   unsigned long previousMillis10ms = 0;
   unsigned long previousMillis100ms = 0;
   unsigned long previousMillis500ms = 0;
+  uint16_t remining_gids = 281;
+  uint8_t mprun10 = 0;
+  uint8_t mprun100 = 0;
+  uint8_t counter_55B = 0;
   CAN_frame LEAF_1DC = {.FD = false,
                         .ext_ID = false,
                         .DLC = 8,
                         .ID = 0x1DC,
-                        .data = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}};
+                        .data = {0x6E, 0x0C, 0x2F, 0xFD, 0x0C, 0x00, 0x00, 0xD8}};
   CAN_frame LEAF_1DB = {.FD = false,
                         .ext_ID = false,
                         .DLC = 8,
@@ -29,22 +33,22 @@ class VCUInverter : public CanInverterProtocol {
                         .ext_ID = false,
                         .DLC = 8,
                         .ID = 0x55B,
-                        .data = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}};
+                        .data = {0x27, 0x00, 0xAA, 0x00, 0xE1, 0x00, 0x10, 0x79}};
   CAN_frame LEAF_5BC = {.FD = false,
                         .ext_ID = false,
                         .DLC = 8,
                         .ID = 0x5BC,
-                        .data = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}};
+                        .data = {0x16, 0x00, 0x14, 0x50, 0xC8, 0x02, 0xA1, 0x68}};
   CAN_frame LEAF_59E = {.FD = false,
                         .ext_ID = false,
                         .DLC = 8,
                         .ID = 0x59E,
-                        .data = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}};
+                        .data = {0x00, 0x00, 0x0E, 0x60, 0x88, 0x00, 0x00, 0x00}};
   CAN_frame LEAF_5C0 = {.FD = false,
                         .ext_ID = false,
                         .DLC = 8,
                         .ID = 0x5C0,
-                        .data = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}};
+                        .data = {0x80, 0x60, 0x60, 0x00, 0xC7, 0xB4, 0x04, 0x0C}};
 };
 
 #endif
