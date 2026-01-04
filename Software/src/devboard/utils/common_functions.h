31e336a13a499d5de15536b967f9ea2202cc309d
Daniel Öster <dalathegreat@gmail.com>
2026-01-04 11:02:10 +0200

    Unify CRC map storage

--- Software/src/devboard/utils/common_functions.h
+++ Software/src/devboard/utils/common_functions.h
@@ -9,3 +9,7 @@
  * 
  */
 extern int16_t sign_extend_to_int16(uint16_t input, unsigned input_bit_width);
+
+extern const uint8_t crc8_table_SAE_J1850_ZER0[256];
+extern const uint8_t crctable_nissan_leaf[256];
+extern const uint8_t crctable_geely_geometryC[256];
