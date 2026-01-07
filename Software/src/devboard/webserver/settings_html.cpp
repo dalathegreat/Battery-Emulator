5d5baed30efe6f1719697ea267509db6693dc8bc
Daniel Öster <dalathegreat@gmail.com>
2026-01-07 22:04:49 +0200

    Fix bug preventing SOC estimate on EGMP

--- Software/src/devboard/webserver/settings_html.cpp
+++ Software/src/devboard/webserver/settings_html.cpp
@@ -1070,7 +1070,7 @@ const char* getCANInterfaceName(CAN_Interface interface) {
     }
 
     form .if-socestimated { display: none; } /* Integrations where you can turn on SOC estimation */
-    form[data-battery="16"],
+    form[data-battery="16"] .if-socestimated,
     form[data-battery="41"] .if-socestimated {
       display: contents;
     }
