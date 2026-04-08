f060cf568469d958809523fc2676549a5a0ba5bf
Jonny <jonny@jonnybarker.com>
2026-04-08 14:11:20 +0100

    experiments

--- Software/src/battery/MG-5-BATTERY.h
+++ Software/src/battery/MG-5-BATTERY.h
@@ -9,7 +9,7 @@ class Mg5Battery : public UdsCanBattery {
  public:
   virtual void setup(void);
   virtual void handle_incoming_can_frame(CAN_frame rx_frame);
-  virtual uint16_t handle_pid(uint16_t pid, uint32_t value);
+  virtual uint32_t handle_pid(uint16_t pid, uint32_t value, const uint8_t* data, uint16_t length, UdsStatus status);
   virtual void update_values();
   virtual void update_soc(uint16_t soc_times_ten);
   virtual void transmit_can(unsigned long currentMillis);
