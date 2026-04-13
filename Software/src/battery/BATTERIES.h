07fc9d4fe3cc59b02d31035162f1ecfbc462fd5e
Daniel Öster <dalathegreat@gmail.com>
2026-04-13 22:14:27 +0300

    Add option to force MysteryVan / normal ECMP mode

--- Software/src/battery/BATTERIES.h
+++ Software/src/battery/BATTERIES.h
@@ -42,6 +42,8 @@ extern uint16_t user_selected_tesla_GTW_mapRegion;
 extern uint16_t user_selected_tesla_GTW_chassisType;
 extern uint16_t user_selected_tesla_GTW_packEnergy;
 extern uint16_t user_selected_pylon_baudrate;
+extern bool user_selected_ECMP_mode;
+extern bool user_selected_MysteryVan_mode;
 
 /* User-selected DALY BMS settings */
 extern int user_selected_daly_power_per_percent;
