531645072f0d6541ae152ffaeeaa48fee8e9b3b9
Jonny <jonny@jonnybarker.com>
2026-04-22 08:14:54 +0100

    Improve mocking for testing

--- test/emul/time.cpp
+++ test/emul/time.cpp
@@ -1,19 +1,28 @@
 #include <stdint.h>
+#include <time.h>
 
 uint64_t current_time = 0;
-
-unsigned long millis() {
-  return static_cast<unsigned long>(current_time);
-}
-
-uint64_t get_timestamp(unsigned long millis) {
-  return 0;
-}
+uint64_t start_time = 0;
 
 uint64_t millis64(void) {
+  if (current_time == UINT64_MAX) {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (ts.tv_sec * 1000 + ts.tv_nsec / 1000000) - start_time;
+  }
+
   return current_time;
+}
+
+unsigned long millis() {
+  return static_cast<unsigned long>(millis64());
 }
 
 void set_millis64(uint64_t time) {
   current_time = time;
+  if (time == UINT64_MAX) {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    start_time = ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+  }
 }
