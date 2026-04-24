5fa926223e0b20b79a3186c82a0489b08f5d35f7
Daniel Öster <dalathegreat@proton.me>
2026-04-24 15:09:57 +0300

    Update Software.cpp

--- Software/Software.cpp
+++ Software/Software.cpp
@@ -39,7 +39,7 @@
 #endif
 
 // The current software version, shown on webserver
-const char* version_number = "10.6.dev";
+const char* version_number = "10.6.0";
 
 // Interval timers
 volatile unsigned long currentMillis = 0;
