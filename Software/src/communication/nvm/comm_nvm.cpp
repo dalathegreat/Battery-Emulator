f1fd34548b4eec0de30056bd7aea1680b0fbc86d
H. Árkosi Róbert <robreg@zsurob.hu>
2026-07-06 11:04:32 +0200

    Add function to clear Wi-Fi station settings

--- Software/src/communication/nvm/comm_nvm.cpp
+++ Software/src/communication/nvm/comm_nvm.cpp
@@ -240,6 +240,20 @@ void init_stored_settings() {
   datalayer_extended.bydAtto3_2.auto_calibrate_soc_enabled = settings.getBool("BYDAUTOCALEN2", true);
 }
 
+void clear_wifi_sta_settings() {
+  BatteryEmulatorSettingsStore settings;
+  static const char* sta_keys[] = {
+      "SSID",     "PASSWORD",
+      "WIFICHANNEL", "STATICIP",
+      "LOCALIP1", "LOCALIP2", "LOCALIP3", "LOCALIP4",
+      "GATEWAY1", "GATEWAY2", "GATEWAY3", "GATEWAY4",
+      "SUBNET1",  "SUBNET2",  "SUBNET3",  "SUBNET4",
+  };
+  for (auto key : sta_keys) {
+    settings.removeKey(key);
+  }
+}
+
 void store_settings_equipment_stop() {
   BatteryEmulatorSettingsStore settings(false);
   settings.saveBool("EQUIPMENT_STOP", datalayer.system.info.equipment_stop_active);
