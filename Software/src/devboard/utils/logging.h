1dfe8d35fbadbe3b3047de7d5a44e3ec4c0cf9d0
H. Árkosi Róbert <robreg@zsurob.hu>
2026-07-12 13:52:57 +0200

    Add syslog_backlog_flush function declaration

--- Software/src/devboard/utils/logging.h
+++ Software/src/devboard/utils/logging.h
@@ -133,6 +133,7 @@ extern Logging logging;
 extern std::string syslog_ip;
 extern uint16_t syslog_port;
 extern uint8_t syslog_facility;  // 0..23
+void syslog_backlog_flush(void);
 #endif
 
 #endif  // __LOGGING_H__
