86e3761982a2a7e83343f4644ee39e88cf5a4945
Jonny <jonny@jonnybarker.com>
2026-08-04 18:41:10 +0100

    Demo Renault Zoe Gen1 migration to UdsCanBattery

--- Software/src/battery/RENAULT-ZOE-GEN1-BATTERY.h
+++ Software/src/battery/RENAULT-ZOE-GEN1-BATTERY.h
@@ -2,19 +2,15 @@
 #define RENAULT_ZOE_GEN1_BATTERY_H
 
 #include "../datalayer/datalayer.h"
-#include "../datalayer/datalayer_extended.h"
-#include "CanBattery.h"
-#include "RENAULT-ZOE-GEN1-HTML.h"
+#include "UdsCanBattery.h"
 
-class RenaultZoeGen1Battery : public CanBattery {
+class RenaultZoeGen1Battery : public UdsCanBattery {
  public:
   // Use this constructor for the second battery.
-  RenaultZoeGen1Battery(DATALAYER_BATTERY_TYPE* datalayer_ptr, DATALAYER_INFO_ZOE* extended, CAN_Interface targetCan)
-      : CanBattery(targetCan) {
+  RenaultZoeGen1Battery(DATALAYER_BATTERY_TYPE* datalayer_ptr, CAN_Interface targetCan) : UdsCanBattery(targetCan) {
     datalayer_battery = datalayer_ptr;
     allows_contactor_closing = nullptr;
-    datalayer_zoe = extended;
-
+    dtc = &datalayer_battery->dtc;
     calculated_total_pack_voltage_mV = 0;
   }
 
@@ -22,7 +18,7 @@ class RenaultZoeGen1Battery : public CanBattery {
   RenaultZoeGen1Battery() {
     datalayer_battery = &datalayer.battery;
     allows_contactor_closing = &datalayer.system.status.battery_allows_contactor_closing;
-    datalayer_zoe = &datalayer_extended.zoe;
+    dtc = &datalayer_battery->dtc;
   }
 
   virtual void setup(void);
@@ -31,15 +27,19 @@ class RenaultZoeGen1Battery : public CanBattery {
   virtual void transmit_can(unsigned long currentMillis);
   static constexpr const char* Name = "Renault Zoe Gen1 22/40kWh";
 
-  BatteryHtmlRenderer& get_status_renderer() { return renderer; }
+  String get_uds_info_html() override;
 
-  bool supports_reset_DTC() { return true; }
-  void reset_DTC() { UserRequestedDTCReset = true; }
+ protected:
+  // Called by the UDS superclass for every successful PID response. `data`
+  // points at the raw value bytes, starting right after the echoed local
+  // identifier. Return 0 to continue the scan list in order.
+  uint16_t handle_pid(uint16_t pid, uint32_t value, const uint8_t* data, uint16_t length) override;
 
  private:
-  RenaultZoeGen1HtmlRenderer renderer;
+  DATALAYER_BATTERY_TYPE* datalayer_battery;
 
-  bool UserRequestedDTCReset = false;
+  // If not null, this battery decides when the contactor can be closed and writes the value here.
+  bool* allows_contactor_closing;
 
   static const int MAX_PACK_VOLTAGE_DV = 4040;  //5000 = 500.0V
   static const int MIN_PACK_VOLTAGE_DV = 3000;
@@ -47,14 +47,15 @@ class RenaultZoeGen1Battery : public CanBattery {
   static const int MAX_CELL_VOLTAGE_MV = 4220;  //Battery is put into emergency stop if one cell goes over this value
   static const int MIN_CELL_VOLTAGE_MV = 2700;  //Battery is put into emergency stop if one cell goes below this value
 
-  DATALAYER_BATTERY_TYPE* datalayer_battery;
-  DATALAYER_INFO_ZOE* datalayer_zoe;
-
-  // If not null, this battery decides when the contactor can be closed and writes the value here.
-  bool* allows_contactor_closing;
+  // One-byte KWP2000 local identifiers polled from the BMS (0x21 service).
+  static const int GROUP1_CELLVOLTAGES_1_POLL = 0x41;  // Cells 1-62
+  static const int GROUP2_CELLVOLTAGES_2_POLL = 0x42;  // Cells 63-96
+  static const int GROUP3_METRICS = 0x61;              // Mileage + alltime energy
+  static const int GROUP4_SOC = 0x03;                  // SOC
+  static const int GROUP5_TEMPERATURE_POLL = 0x04;     // Cell temperatures
+  static const int GROUP6_BALANCING = 0x07;            // Balancing status bits
 
   unsigned long previousMillis100 = 0;  // will store last time a 100ms CAN Message was sent
-  unsigned long previousMillis250 = 0;  // will store last time a 250ms CAN Message was sent
   uint8_t counter_423 = 0;
 
   CAN_frame ZOE_423 = {.FD = false,
@@ -62,28 +63,6 @@ class RenaultZoeGen1Battery : public CanBattery {
                        .DLC = 8,
                        .ID = 0x423,
                        .data = {0x07, 0x1d, 0x00, 0x02, 0x5d, 0x80, 0x5d, 0xc8}};
-  CAN_frame ZOE_POLL_79B = {.FD = false,
-                            .ext_ID = false,
-                            .DLC = 8,
-                            .ID = 0x79B,
-                            .data = {0x02, 0x21, 0x41, 0x00, 0x00, 0x00, 0x00, 0x00}};
-  CAN_frame ZOE_ACK_79B = {.FD = false,
-                           .ext_ID = false,
-                           .DLC = 8,
-                           .ID = 0x79B,
-                           .data = {0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}};
-  CAN_frame ZOE_CLEAR_DTC = {.FD = false,
-                             .ext_ID = false,
-                             .DLC = 8,
-                             .ID = 0x79B,
-                             .data = {0x04, 0x14, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00}};
-
-#define GROUP1_CELLVOLTAGES_1_POLL 0x41
-#define GROUP2_CELLVOLTAGES_2_POLL 0x42
-#define GROUP3_METRICS 0x61
-#define GROUP4_SOC 0x03
-#define GROUP5_TEMPERATURE_POLL 0x04
-#define GROUP6_BALANCING 0x07
 
   uint16_t LB_SOC = 50;
   uint16_t LB_Display_SOC = 50;
@@ -108,13 +87,7 @@ class RenaultZoeGen1Battery : public CanBattery {
   uint8_t LB_HVBOT = 0;
   uint8_t LB_HVBOV = 0;
   uint8_t LB_COV = 0;
-  uint8_t frame0 = 0;
-  uint8_t current_poll = 0;
-  uint8_t requested_poll = 0;
-  uint8_t group = 0;
-  uint16_t cellvoltages[96];
   uint32_t calculated_total_pack_voltage_mV = 370000;
-  uint8_t highbyte_cell_next_frame = 0;
   uint16_t SOC_polled = 5000;
   int16_t cell_1_temperature_polled = 0;
   int16_t cell_2_temperature_polled = 0;
@@ -130,7 +103,6 @@ class RenaultZoeGen1Battery : public CanBattery {
   int16_t cell_12_temperature_polled = 0;
   uint16_t battery_mileage_in_km = 0;
   uint16_t kWh_from_beginning_of_battery_life = 0;
-  bool looping_over_20 = false;
 };
 
 #endif
