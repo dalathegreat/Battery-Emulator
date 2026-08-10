a7a317b945ebaf4096e6d709d0e44aa3d96c3443
H. Árkosi Róbert <robreg@zsurob.hu>
2026-08-10 10:59:31 +0200

    Fix typo in battery HTML header from 'HX' to 'Hx'

--- Software/src/battery/NISSAN-LEAF-HTML.h
+++ Software/src/battery/NISSAN-LEAF-HTML.h
@@ -47,7 +47,7 @@ class NissanLeafHtmlRenderer : public BatteryHtmlRenderer {
     content += "<h4>BMS ID: " + String(readableBMSID) + "</h4>";
     content += "<h4>GIDS: " + String(nissan_dl->GIDS) + "</h4>";
     content +=
-        "<h4>HX: " +
+        "<h4>Hx: " +
         (nissan_dl->battery_HX_pptt ? String(nissan_dl->battery_HX_pptt / 100.0f, 2) + " %" : String("Unknown")) +
         "</h4>";
     content += "<h4>Regen kW: " + String(nissan_dl->ChargePowerLimit) + "</h4>";
