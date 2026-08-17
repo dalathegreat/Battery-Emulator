75443689e1eacdf5f9c3130316c7a1e12a655555
Daniel Öster <dalathegreat@gmail.com>
2026-08-18 00:22:58 +0300

    Update text

--- Software/src/battery/STELLANTIS-PRO-ONE-BATTERY.cpp
+++ Software/src/battery/STELLANTIS-PRO-ONE-BATTERY.cpp
@@ -90,11 +90,11 @@ String StellantisProOneBattery::get_uds_info_html() {
   content << "<h4>BMS HW version number: " << pid_hw_version_num << "</h4>"
               "<h4>SW Homologation Code: " << hexToAscii(pid_sw_homologation_code).c_str() << "</h4>"
               "<h4>Unknown12: " << pid_unknown_12 << "</h4>"
-              "<h4>Unknown12: " << pid_unknown_178 << "</h4>"
-              "<h4>Unknown12: " << pid_unknown_179 << "</h4>"
-              "<h4>Unknown12: " << pid_unknown_180 << "</h4>"
-              "<h4>Unknown12: " << pid_unknown_181 << "</h4>"
-              "<h4>Unknown12: " << pid_unknown_182 << "</h4>"
+              "<h4>Unknown178: " << pid_unknown_178 << "</h4>"
+              "<h4>Unknown179: " << pid_unknown_179 << "</h4>"
+              "<h4>Unknown180: " << pid_unknown_180 << "</h4>"
+              "<h4>Unknown181: " << pid_unknown_181 << "</h4>"
+              "<h4>Unknown182: " << pid_unknown_182 << "</h4>"
               "<h4>Temperature sensors: </h4>"
            "<table style='border-collapse:collapse;font-size:0.85em;margin:auto'>";
 
