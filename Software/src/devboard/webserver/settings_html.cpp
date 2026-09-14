193167178812ec45ae4ce1c4cbb1445a83a6801c
Daniel Öster <dalathegreat@gmail.com>
2026-09-14 23:03:03 +0300

    Update enum for kW modification

--- Software/src/devboard/webserver/settings_html.cpp
+++ Software/src/devboard/webserver/settings_html.cpp
@@ -1498,7 +1498,7 @@ const char* getCANInterfaceName(CAN_Interface interface) {
     form[data-battery="44"] .if-estimated,
     form[data-battery="50"] .if-estimated,
     form[data-battery="51"] .if-estimated,
-    form[data-battery="56"] .if-estimated {
+    form[data-battery="58"] .if-estimated {
       display: contents;
     }
 
