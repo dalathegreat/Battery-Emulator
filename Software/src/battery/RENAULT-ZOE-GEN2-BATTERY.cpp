b0a415ee3017fb002edb948e95bff3a9098c44c2
Daniel Öster <dalathegreat@gmail.com>
2026-09-26 23:39:46 +0300

    Tidy up PID scan list, speedup current read

--- Software/src/battery/RENAULT-ZOE-GEN2-BATTERY.cpp
+++ Software/src/battery/RENAULT-ZOE-GEN2-BATTERY.cpp
@@ -425,6 +425,12 @@ uint16_t RenaultZoeGen2Battery::handle_pid(uint16_t pid, uint32_t value, const u
       }
       break;
   }
+
+  // Detour to POLL_CURRENT every other scan since we need it at a fast update rate
+  if (pid != POLL_CURRENT) {
+    return POLL_CURRENT;
+  }
+
   return 0;  //Continue scanning the PID list in order
 }
 
@@ -501,162 +507,135 @@ void RenaultZoeGen2Battery::setup(void) {  // Performs one time setup at startup
       POLL_USABLE_SOC,
       POLL_SOH,
       POLL_PACK_VOLTAGE,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_MAX_CELL_VOLTAGE,
       POLL_MIN_CELL_VOLTAGE,
       POLL_12V,
       POLL_AVG_TEMP,
       POLL_MIN_TEMP,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_MAX_TEMP,
       POLL_MAX_POWER,
       POLL_INTERLOCK,
       POLL_KWH,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CURRENT_OFFSET,
       POLL_MAX_GENERATED,
       POLL_MAX_AVAILABLE,
       POLL_CURRENT_VOLTAGE,
       POLL_CHARGING_STATUS,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_REMAINING_CHARGE,
       POLL_BALANCE_CAPACITY_TOTAL,
       POLL_BALANCE_TIME_TOTAL,
       POLL_BALANCE_CAPACITY_SLEEP,
       POLL_BALANCE_TIME_SLEEP,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_BALANCE_CAPACITY_WAKE,
       POLL_BALANCE_TIME_WAKE,
       POLL_BMS_STATE,
       POLL_BALANCE_SWITCHES,
       POLL_ENERGY_COMPLETE,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_ENERGY_PARTIAL,
       POLL_SLAVE_FAILURES,
       POLL_MILEAGE,
       POLL_FAN_SPEED,
       POLL_FAN_PERIOD,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_FAN_CONTROL,
       POLL_FAN_DUTY,
       POLL_TEMPORISATION,
       POLL_TIME,
       POLL_PACK_TIME,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_SOC_MIN,
       POLL_SOC_MAX,
       POLL_CELL_0,
       POLL_CELL_1,
       POLL_CELL_2,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_3,
       POLL_CELL_4,
       POLL_CELL_5,
       POLL_CELL_6,
       POLL_CELL_7,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_8,
       POLL_CELL_9,
       POLL_CELL_10,
       POLL_CELL_11,
       POLL_CELL_12,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_13,
       POLL_CELL_14,
       POLL_CELL_15,
       POLL_CELL_16,
       POLL_CELL_17,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_18,
       POLL_CELL_19,
       POLL_CELL_20,
       POLL_CELL_21,
       POLL_CELL_22,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_23,
       POLL_CELL_24,
       POLL_CELL_25,
       POLL_CELL_26,
       POLL_CELL_27,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_28,
       POLL_CELL_29,
       POLL_CELL_30,
       POLL_CELL_31,
       POLL_CELL_32,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_33,
       POLL_CELL_34,
       POLL_CELL_35,
       POLL_CELL_36,
       POLL_CELL_37,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_38,
       POLL_CELL_39,
       POLL_CELL_40,
       POLL_CELL_41,
       POLL_CELL_42,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_43,
       POLL_CELL_44,
       POLL_CELL_45,
       POLL_CELL_46,
       POLL_CELL_47,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_48,
       POLL_CELL_49,
       POLL_CELL_50,
       POLL_CELL_51,
       POLL_CELL_52,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_53,
       POLL_CELL_54,
       POLL_CELL_55,
       POLL_CELL_56,
       POLL_CELL_57,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_58,
       POLL_CELL_59,
       POLL_CELL_60,
       POLL_CELL_61,
       POLL_CELL_62,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_63,
       POLL_CELL_64,
       POLL_CELL_65,
       POLL_CELL_66,
       POLL_CELL_67,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_68,
       POLL_CELL_69,
       POLL_CELL_70,
       POLL_CELL_71,
       POLL_CELL_72,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_73,
       POLL_CELL_74,
       POLL_CELL_75,
       POLL_CELL_76,
       POLL_CELL_77,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_78,
       POLL_CELL_79,
       POLL_CELL_80,
       POLL_CELL_81,
       POLL_CELL_82,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_83,
       POLL_CELL_84,
       POLL_CELL_85,
       POLL_CELL_86,
       POLL_CELL_87,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_88,
       POLL_CELL_89,
       POLL_CELL_90,
       POLL_CELL_91,
       POLL_CELL_92,
-      POLL_CURRENT,  //Repeated to speed up update rate on this critical measurement
       POLL_CELL_93,
       POLL_CELL_94,
       POLL_CELL_95,
